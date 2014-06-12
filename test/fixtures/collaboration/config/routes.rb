3ce465aa9fc8f5ac2f70ec121ce4e303cda4c7a9
Luca Guidi <me@lucaguidi.com>
2014-06-12 12:11:14 +0200

    Handle thrown statuses from actions

--- test/fixtures/collaboration/config/routes.rb
+++ test/fixtures/collaboration/config/routes.rb
@@ -1,4 +1,5 @@
 get '/',              to: 'home#index', as: :root
 get '/error',         to: 'home#error'
 get '/action_legacy', to: 'home#legacy'
+get '/protected',     to: 'protected#index'
 redirect '/legacy',   to: '/'
