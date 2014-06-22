35240d3029081f3b5779de510a5b11ad4ede500b
Luca Guidi <me@lucaguidi.com>
2014-06-22 17:11:38 +0200

    Let Config::LoadPaths to use the right subclassing semantic of Utils::LoadPaths

--- lib/lotus/config/load_paths.rb
+++ lib/lotus/config/load_paths.rb
@@ -4,23 +4,22 @@ module Lotus
   module Config
     # Define the load paths where the application should load
     #
-    # #since 0.1.0
+    # @since 0.1.0
     # @api private
     class LoadPaths < Utils::LoadPaths
       PATTERN = '**/*.rb'.freeze
 
       def load!(root)
-        each(root) do |path|
+        @root = root
+
+        each do |path|
           Dir.glob(path.join(PATTERN)).each {|file| require file }
         end
       end
 
       protected
-      def each(root, &blk)
-        # FIXME implement #realpath in Utils::LoadPaths
-        Utils::Kernel.Array(@paths).each do |path|
-          blk.call root.join(path).realpath
-        end
+      def realpath(path)
+        @root.join(path).realpath
       end
     end
   end
