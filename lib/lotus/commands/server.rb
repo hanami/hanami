b55ed4e1d88914e81c1a95716bf12e6fce067472
Leonard Garvey <lengarvey@gmail.com>
2014-06-27 22:40:00 +1000

    Remove ::Rack::Chunked from middleware.
    Rack::Chunked causes Safari to not load our pages.
    Keeps Rack::ShowExceptions and Rack::Lint in development as per racks
      defaults.
    
    It might be worth pointing out that Rails doesn't use Rack::Chunked
    either which is why it is unaffected by this Safari/Rack issue.
    
    Fixes #5

--- lib/lotus/commands/server.rb
+++ lib/lotus/commands/server.rb
@@ -6,6 +6,15 @@ module Lotus
       def options
         @options ||= parse_options(ARGV.slice(1..-1))
       end
+
+      # Primarily this removes the ::Rack::Chunked middleware
+      # which is the cause of Safari content-length bugs.
+      def middleware
+        m = Hash.new { |e, m| e[m] = [] }
+        m["deployment"].concat([::Rack::ContentLength])
+        m["development"].concat(m["deployment"] + [::Rack::ShowExceptions, ::Rack::Lint])
+        m
+      end
     end
   end
 end
--- test/commands_server_test.rb
+++ test/commands_server_test.rb
@@ -23,4 +23,20 @@ describe Lotus::Commands::Server do
       @server.options[:Port].must_equal "3005"
     end
   end
+
+  describe '#middleware' do
+    it 'should not have ShowExceptions in deployment' do
+      @server.middleware["deployment"]
+        .include?(::Rack::ShowExceptions).must_equal false
+      @server.middleware["development"]
+        .include?(::Rack::ShowExceptions).must_equal true
+    end
+
+    it 'should have ContentLength loaded' do
+      @server.middleware["development"]
+        .include?(::Rack::ContentLength).must_equal true
+      @server.middleware["deployment"]
+        .include?(::Rack::ContentLength).must_equal true
+    end
+  end
 end
