311b94786870a74ce02fcd5721f27555773d87b5
Byron Bischoff <byronb@gmail.com>
2014-07-08 16:24:34 -0700

    Fix Lotus::Frameworks::Action to call super and append self for #response

--- lib/lotus/frameworks.rb
+++ lib/lotus/frameworks.rb
@@ -2,34 +2,22 @@ require 'lotus/router'
 require 'lotus/controller'
 require 'lotus/view'
 
-# FIXME Ideally, this should be done like this:
-#
-# module Lotus
-#   module Frameworks
-#     module Action
-#       module Rack
-#         protected
-#         def response
-#           [super, self].flatten
-#         end
-#       end
-#     end
-#   end
-# end
-#
-# Lotus::Action::Rack.class_eval do
-#   include Lotus::Frameworks::Action::Rack
-# end
-#
-# ..but it doesn't work and I want to ship it!
+module Lotus
+  module Frameworks
+    module Action
+      module Rack
+        protected
+        def response
+          super + [self]
+        end
+      end
+    end
+  end
+end
 
 Lotus::Action::Rack.class_eval do
-  DEFAULT_RESPONSE_CODE = 200
-  DEFAULT_RESPONSE_BODY = []
-
-  protected
-  def response
-    [ @_status || DEFAULT_RESPONSE_CODE, headers, @_body || DEFAULT_RESPONSE_BODY.dup, self ]
+  def self.included(base)
+    base.include Lotus::Frameworks::Action::Rack
   end
 end
 
