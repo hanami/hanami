7da7370146607d08db23e3ee69dd3ecc2511c1f6
josue.abreu@gmail.com <josue.abreu@gmail.com>
2014-07-15 00:11:15 -0400

    `Configuration#cookies` integration tests and fixture.

---
+++ test/fixtures/cookies/application.rb
@@ -0,0 +1,59 @@
+module CookiesApp
+  class Application < Lotus::Application
+    configure do
+      # Activate Cookies
+      cookies true
+
+      routes do
+        get '/get_cookies',              to: 'cookies#get'
+        get '/set_cookies',              to: 'cookies#set'
+        get '/set_cookies_with_options', to: 'cookies#set_with_options'
+        get '/del_cookies',              to: 'cookies#del'
+      end
+    end
+
+    load!
+  end
+
+
+  module Controllers::Cookies
+    include CookiesApp::Controller
+
+    action 'Get' do
+      def call(params)
+        self.body = cookies[:foo]
+      end
+    end
+
+    action 'Set' do
+      def call(params)
+        self.body = 'yummy!'
+        cookies[:big_cookie] = 'nomnomnom!'
+      end
+    end
+
+    action 'SetWithOptions' do
+      def call(params)
+        self.body = 'with options!'
+        expire_date = Time.parse params[:expires]
+
+        cookies[:with_options] = {
+          value: 'nomnomnom!',
+          domain: 'lotusrocks.com',
+          path: '/controller',
+          expires: expire_date,
+          secure: true,
+          httponly: true
+        }
+      end
+    end
+
+    action 'Del' do
+      def call(params)
+        self.body = 'deleted!'
+        cookies[:delete] = nil
+      end
+    end
+  end
+
+end
---
+++ test/integration/cookies_test.rb
@@ -0,0 +1,64 @@
+require 'test_helper'
+require 'rack/test'
+require 'fixtures/cookies/application'
+
+describe 'Cookies' do
+  include Rack::Test::Methods
+
+  before do
+    @current_dir = Dir.pwd
+    Dir.chdir FIXTURES_ROOT.join('cookies')
+    @app = CookiesApp::Application.new
+  end
+
+  after do
+    Dir.chdir @current_dir
+    @current_dir = nil
+  end
+
+  def app
+    @app
+  end
+
+  def response
+    last_response
+  end
+
+  def request
+    last_request
+  end
+
+  it 'sucessfully gets cookies' do
+    get '/get_cookies', nil, {'HTTP_COOKIE' => 'foo=bar'}
+
+    request.cookies.must_equal({ 'foo' => 'bar' })
+
+    response.body.must_equal('bar')
+    response.headers['Set-Cookie'].must_equal('foo=bar')
+  end
+
+  it 'succesfully sets cookies' do
+    get '/set_cookies'
+
+    response.body.must_equal('yummy!')
+    response.headers['Set-Cookie'].must_equal('big_cookie=nomnomnom%21')
+  end
+
+  it 'sucessfully sets cookies with options' do
+    next_week = Time.now + 60 * 60 * 24 * 7
+    get '/set_cookies_with_options', { expires: next_week }
+
+    response.body.must_equal('with options!')
+    response.headers['Set-Cookie'].must_equal("with_options=nomnomnom%21; domain=lotusrocks.com; path=/controller; expires=#{next_week.gmtime.rfc2822}; secure; HttpOnly")
+  end
+
+  it 'sucessfully deletes cookies' do
+    get '/del_cookies', nil, {'HTTP_COOKIE' => 'foo=bar;delete=cookie'}
+
+    request.cookies.must_equal({ 'foo' => 'bar', 'delete' => 'cookie' })
+
+    response.body.must_equal('deleted!')
+    response.headers['Set-Cookie'].must_equal("foo=bar\ndelete=; max-age=0; expires=Thu, 01 Jan 1970 00:00:00 -0000")
+  end
+
+end
