c0c16cc4bcf56ff5af1a2f430066f6718222a253
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2014-10-29 21:40:57 -0400

    Adding a some documentation to Environment
    The methods of Lotus::Environment are somewhat self-evident.

--- lib/lotus/environment.rb
+++ lib/lotus/environment.rb
@@ -2,6 +2,9 @@ require 'thread'
 require 'lotus/utils/hash'
 
 module Lotus
+  # Define and expose information about the Lotus environment.
+  #
+  # @since 0.1.0
   class Environment
     RACK_ENV    = 'RACK_ENV'.freeze
     LOTUS_ENV   = 'LOTUS_ENV'.freeze
@@ -17,34 +20,49 @@ module Lotus
     DEFAULT_CONFIG       = 'config.ru'.freeze
     DEFAULT_APPLICATIONS = 'config/applications'.freeze
 
+    # @param options [Hash] override default options for various environment attributes
     def initialize(options = {})
       @options = Utils::Hash.new(options).symbolize!.freeze
       @mutex   = Mutex.new
       @mutex.synchronize { set_env_vars! }
     end
 
+    # In what context are the applications running?
+    #
+    # @return [String]
     def environment
       @environment ||= ENV[LOTUS_ENV] || ENV[RACK_ENV] || DEFAULT_ENV
     end
 
+    # The HTTP host name
+    #
+    # @return [String]
     def host
       @host ||= @options.fetch(:host) {
         ENV[LOTUS_HOST] || default_host
       }
     end
 
+    # The HTTP port
+    #
+    # @return [Integer]
     def port
       @port ||= @options.fetch(:port) { ENV[LOTUS_PORT] || DEFAULT_PORT }.to_i
     end
 
+    # Filename of the Rack configuration file
+    #
+    # @return [String] filename with the Rack configuration
     def config
       @options.fetch(:config) { DEFAULT_CONFIG }
     end
 
+    # @return [String] path to directory that contains registered applications
     def applications
       @options.fetch(:applications) { DEFAULT_APPLICATIONS }
     end
 
+    # @return [Lotus::Utils::Hash]
     def to_options
       @options.merge(
         environment:  environment,
