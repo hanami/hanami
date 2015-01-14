9a7f7410197a83a6e79bb47b2adc8e8e64b9f356
Rob Yurkowski <rob@yurkowski.net>
2015-01-14 14:56:46 -0500

    Add ApplicationName class, fixing dasherized application names and prohibiting use of reserved words. (Fixes #129)

--- lib/lotus/generators/slice.rb
+++ lib/lotus/generators/slice.rb
@@ -1,6 +1,7 @@
 require 'securerandom'
 require 'lotus/generators/abstract'
 require 'lotus/utils/string'
+require 'lotus/application_name'
 
 module Lotus
   module Generators
@@ -8,8 +9,9 @@ module Lotus
       def initialize(command)
         super
 
-        @slice_name            = options.fetch(:application)
-        @upcase_slice_name     = @slice_name.upcase
+        application_name       = ApplicationName.new(options.fetch(:application))
+        @slice_name            = application_name.to_s
+        @upcase_slice_name     = application_name.to_env_s
         @classified_slice_name = Utils::String.new(@slice_name).classify
 
         @source                = Pathname.new(::File.dirname(__FILE__) + '/../generators/slice')
