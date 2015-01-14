66e521d8d2092fd83ff0c138cccb62cae8ef0eb3
Trung Lê <trung.le@ruby-journal.com>
2015-01-15 00:22:42 +1100

    Generate lib/config/mapping.rb

--- test/commands/new_test.rb
+++ test/commands/new_test.rb
@@ -167,6 +167,19 @@ describe Lotus::Commands::New do
         content.must_match %(Lotus::Model.configure)
         content.must_match %(adapter type: :file_system, uri: ENV['CHIRP_DATABASE_URL'])
         content.must_match %(mapping do)
+        content.must_match %(mapping "{__dir__}/config/mapping")
+      end
+    end
+
+    describe 'lib/config/mapping.rb' do
+      it 'generates it' do
+        content = @root.join('lib/config/mapping.rb').read
+        content.must_match %(collection :users do)
+        content.must_match %(entity     User)
+        content.must_match %(repository UserRepository)
+        content.must_match %(attribute :id,   Integer)
+        content.must_match %(attribute :name, String)
+        content.must_match %(end)
       end
     end
 
