AlfonsoUceda/lotus

Added security policies for headers

Gemfile
@@ -9,7 +9,7 @@ end
 gem 'lotus-utils',       '~> 0.3', require: false, github: 'lotus/utils',       branch: '0.3.x'
 gem 'lotus-validations', '~> 0.2', require: false, github: 'lotus/validations', branch: '0.2.x'
 gem 'lotus-router',      '~> 0.2', require: false, github: 'lotus/router',      branch: '0.2.x'
-gem 'lotus-controller',  '~> 0.3', require: false, github: 'lotus/controller',  branch: '0.3.x'
+gem 'lotus-controller',  '~> 0.3', require: false, github: 'alfonsouceda/controller',  branch: 'security_default_headers'
 gem 'lotus-view',        '~> 0.3', require: false, github: 'lotus/view',        branch: '0.3.x'
 gem 'lotus-model',       '~> 0.2', require: false, github: 'lotus/model',       branch: '0.2.x'
 

lib/lotus/config/security.rb
@@ -1,0 +1,80 @@
+module Lotus
+  module Config
+    # Security policies are stored here.
+    #
+    # @since x.x.x
+    class Security
+      # @since x.x.x
+      # @api private
+      #
+      # @see Lotus::Loader#_configure_controller_framework!
+      X_FRAME_OPTIONS_HEADER = 'X-Frame-Options'.freeze
+
+      # @since x.x.x
+      # @api private
+      #
+      # @see Lotus::Config::Security#initialize
+      X_FRAME_OPTIONS_DEFAULT = 'DENY'.freeze
+
+      # @since x.x.x
+      # @api private
+      #
+      # @see Lotus::Loader#_configure_controller_framework!
+      CONTENT_POLICY_SECURITY_HEADER = 'Content-Security-Policy'.freeze
+
+      # @since x.x.x
+      # @api private
+      #
+      # @see Lotus::Config::Security#initialize
+      CONTENT_POLICY_SECURITY_DEFAULT = "connect-src 'self'; script-src 'self';".freeze
+
+      # Set security headers with default values
+      #
+      # @since x.x.x
+      # @api private
+      def initialize
+        @x_frame_options         = X_FRAME_OPTIONS_DEFAULT
+        @content_policy_security = CONTENT_POLICY_SECURITY_DEFAULT
+      end
+
+      # X-Frame-Options headers' value
+      #
+      # @overload x_frame_options(value)
+      #   Sets the given value
+      #   @param value [String] for X-Frame-Options header.
+      #
+      # @overload x_frame_options
+      #   Gets the value
+      #   @return [String] X-Frame-Options header's value
+      #
+      # @since x.x.x
+      def x_frame_options(value = nil)
+        if value.nil?
+          @x_frame_options
+        else
+          @x_frame_options = value
+        end
+      end
+
+
+      # Content-Policy-Security headers' value
+      #
+      # @overload content_policy_security(value)
+      #   Sets the given value
+      #   @param value [String] for Content-Security-Policy header.
+      #
+      # @overload content_policy_security
+      #   Gets the value
+      #   @return [String] Content-Security-Policy header's value
+      #
+      # @since x.x.x
+      def content_policy_security(value = nil)
+        if value.nil?
+          @content_policy_security
+        else
+          @content_policy_security = value
+        end
+      end
+    end
+  end
+end

lib/lotus/loader.rb
@@ -5,6 +5,7 @@ require 'lotus/routes'
 require 'lotus/routing/default'
 require 'lotus/action/cookies'
 require 'lotus/action/session'
+require 'lotus/config/security'
 
 module Lotus
   # Load an application
@@ -49,6 +50,10 @@ module Lotus
         controller = Lotus::Controller.duplicate(namespace) do
           handle_exceptions config.handle_exceptions
           default_format    config.default_format
+          default_headers({
+            Lotus::Config::Security::X_FRAME_OPTIONS_HEADER         => config.security.x_frame_options,
+            Lotus::Config::Security::CONTENT_POLICY_SECURITY_HEADER => config.security.content_policy_security
+          })
 
           prepare { include Lotus::Action::Cookies } if config.cookies
           prepare { include Lotus::Action::Session } if config.sessions.enabled?

test/fixtures/security_headers/apps/web/application.rb
@@ -1,0 +1,40 @@
+require 'lotus'
+require 'lotus/model'
+
+SECURITY_HEADERS_ADAPTER_TYPE =  if RUBY_ENGINE == 'jruby'
+                  require 'jdbc/sqlite3'
+                  Jdbc::SQLite3.load_driver
+                  'jdbc:sqlite'
+                else
+                  require 'sqlite3'
+                  'sqlite'
+                end
+
+require 'lotus/model/adapters/sql_adapter'
+db = Pathname.new(File.dirname(__FILE__)).join('../tmp/test.db')
+db.dirname.mkpath      # create directory if not exist
+db.delete if db.exist? # delete file if exist
+SECURITY_HEADERS_SQLITE_CONNECTION_STRING = "#{SECURITY_HEADERS_ADAPTER_TYPE}://#{ db }"
+
+SECURITY_HEADERS_DB = Sequel.connect(SECURITY_HEADERS_SQLITE_CONNECTION_STRING)
+
+SECURITY_HEADERS_DB.create_table :books do
+  primary_key :id
+  String  :name
+end
+
+module SecurityHeaders
+  class Application < Lotus::Application
+    configure do
+      layout :application
+      load_paths << 'app'
+      routes  'config/routes'
+
+      security.x_frame_options 'ALLOW ALL'
+      security.content_policy_security "script-src 'self' https://apis.google.com"
+
+      adapter type: :sql, uri: SECURITY_HEADERS_SQLITE_CONNECTION_STRING
+      mapping 'config/mapping'
+    end
+  end
+end

hanami/hanami

Gemfile
@@ -9,7 +9,7 @@ end
 gem 'lotus-utils',       '~> 0.3', require: false, github: 'lotus/utils',       branch: '0.3.x'
 gem 'lotus-validations', '~> 0.2', require: false, github: 'lotus/validations', branch: '0.2.x'
 gem 'lotus-router',      '~> 0.2', require: false, github: 'lotus/router',      branch: '0.2.x'
-gem 'lotus-controller',  '~> 0.3', require: false, github: 'lotus/controller',  branch: '0.3.x'
+gem 'lotus-controller',  '~> 0.3', require: false, github: 'lotus/controller',  branch: 'master'
 gem 'lotus-view',        '~> 0.3', require: false, github: 'lotus/view',        branch: '0.3.x'
 gem 'lotus-model',       '~> 0.2', require: false, github: 'lotus/model',       branch: '0.2.x'
 

lib/lotus/config/security.rb
@@ -1,0 +1,80 @@
+module Lotus
+  module Config
+    # Security policies are stored here.
+    #
+    # @since x.x.x
+    class Security
+      # @since x.x.x
+      # @api private
+      #
+      # @see Lotus::Loader#_configure_controller_framework!
+      X_FRAME_OPTIONS_HEADER = 'X-Frame-Options'.freeze
+
+      # @since x.x.x
+      # @api private
+      #
+      # @see Lotus::Config::Security#initialize
+      X_FRAME_OPTIONS_DEFAULT = 'DENY'.freeze
+
+      # @since x.x.x
+      # @api private
+      #
+      # @see Lotus::Loader#_configure_controller_framework!
+      CONTENT_SECURITY_POLICY_HEADER = 'Content-Security-Policy'.freeze
+
+      # @since x.x.x
+      # @api private
+      #
+      # @see Lotus::Config::Security#initialize
+      CONTENT_SECURITY_POLICY_DEFAULT = "connect-src 'self'; script-src 'self';".freeze
+
+      # Set security headers with default values
+      #
+      # @since x.x.x
+      # @api private
+      def initialize
+        @x_frame_options         = X_FRAME_OPTIONS_DEFAULT
+        @content_security_policy = CONTENT_SECURITY_POLICY_DEFAULT
+      end
+
+      # X-Frame-Options headers' value
+      #
+      # @overload x_frame_options(value)
+      #   Sets the given value
+      #   @param value [String] for X-Frame-Options header.
+      #
+      # @overload x_frame_options
+      #   Gets the value
+      #   @return [String] X-Frame-Options header's value
+      #
+      # @since x.x.x
+      def x_frame_options(value = nil)
+        if value.nil?
+          @x_frame_options
+        else
+          @x_frame_options = value
+        end
+      end
+
+
+      # Content-Policy-Security headers' value
+      #
+      # @overload content_security_policy(value)
+      #   Sets the given value
+      #   @param value [String] for Content-Security-Policy header.
+      #
+      # @overload content_security_policy
+      #   Gets the value
+      #   @return [String] Content-Security-Policy header's value
+      #
+      # @since x.x.x
+      def content_security_policy(value = nil)
+        if value.nil?
+          @content_security_policy
+        else
+          @content_security_policy = value
+        end
+      end
+    end
+  end
+end

lib/lotus/loader.rb
@@ -5,6 +5,7 @@ require 'lotus/routes'
 require 'lotus/routing/default'
 require 'lotus/action/cookies'
 require 'lotus/action/session'
+require 'lotus/config/security'
 
 module Lotus
   # Load an application
@@ -49,6 +50,10 @@ module Lotus
         controller = Lotus::Controller.duplicate(namespace) do
           handle_exceptions config.handle_exceptions
           default_format    config.default_format
+          default_headers({
+            Lotus::Config::Security::X_FRAME_OPTIONS_HEADER         => config.security.x_frame_options,
+            Lotus::Config::Security::CONTENT_SECURITY_POLICY_HEADER => config.security.content_security_policy
+          })
 
           prepare { include Lotus::Action::Cookies } if config.cookies
           prepare { include Lotus::Action::Session } if config.sessions.enabled?

test/fixtures/security_headers/apps/web/application.rb
@@ -1,0 +1,40 @@
+require 'lotus'
+require 'lotus/model'
+
+SECURITY_HEADERS_ADAPTER_TYPE =  if RUBY_ENGINE == 'jruby'
+                  require 'jdbc/sqlite3'
+                  Jdbc::SQLite3.load_driver
+                  'jdbc:sqlite'
+                else
+                  require 'sqlite3'
+                  'sqlite'
+                end
+
+require 'lotus/model/adapters/sql_adapter'
+db = Pathname.new(File.dirname(__FILE__)).join('../tmp/test.db')
+db.dirname.mkpath      # create directory if not exist
+db.delete if db.exist? # delete file if exist
+SECURITY_HEADERS_SQLITE_CONNECTION_STRING = "#{SECURITY_HEADERS_ADAPTER_TYPE}://#{ db }"
+
+SECURITY_HEADERS_DB = Sequel.connect(SECURITY_HEADERS_SQLITE_CONNECTION_STRING)
+
+SECURITY_HEADERS_DB.create_table :books do
+  primary_key :id
+  String  :name
+end
+
+module SecurityHeaders
+  class Application < Lotus::Application
+    configure do
+      layout :application
+      load_paths << 'app'
+      routes  'config/routes'
+
+      security.x_frame_options 'ALLOW ALL'
+      security.content_security_policy "script-src 'self' https://apis.google.com"
+
+      adapter type: :sql, uri: SECURITY_HEADERS_SQLITE_CONNECTION_STRING
+      mapping 'config/mapping'
+    end
+  end
+end