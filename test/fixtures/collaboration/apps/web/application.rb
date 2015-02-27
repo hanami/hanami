fac936e0c5e115dab13457210cad541704c41527
Vínicius Oliveira <oliveira.mvs@gmail.com>
2015-02-27 01:26:40 -0300

    Sorry should haven't committed this.

--- test/fixtures/collaboration/apps/web/application.rb
+++ test/fixtures/collaboration/apps/web/application.rb
@@ -1,4 +1,21 @@
-require_relative '../../../sql_adapter'
+require 'lotus'
+require 'lotus/model'
+
+ADAPTER_TYPE =  if RUBY_ENGINE == 'jruby'
+                  require 'jdbc/sqlite3'
+                  Jdbc::SQLite3.load_driver
+                  'jdbc:sqlite'
+                else
+                  require 'sqlite3'
+                  'sqlite'
+                end
+
+require 'lotus/model/adapters/sql_adapter'
+db = Pathname.new(File.dirname(__FILE__)).join('../tmp/test.db')
+db.dirname.mkpath      # create directory if not exist
+db.delete if db.exist? # delete file if exist
+SQLITE_CONNECTION_STRING = "#{ADAPTER_TYPE}://#{ db }"
+
 DB = Sequel.connect(SQLITE_CONNECTION_STRING)
 
 DB.create_table :books do
