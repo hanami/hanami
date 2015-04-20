a29ae905b99b992bfafb2d68d7327aff38141069
Ngoc Nguyen <nguyenngoc2505@gmail.com>
2015-04-20 14:18:10 +0700

    issue#211 Add Lotus.env

--- lib/lotus.rb
+++ lib/lotus.rb
@@ -2,6 +2,7 @@ require 'lotus/version'
 require 'lotus/application'
 require 'lotus/container'
 require 'lotus/logger'
+require 'lotus/environment'
 
 # A complete web framework for Ruby
 #
@@ -9,4 +10,22 @@ require 'lotus/logger'
 #
 # @see http://lotusrb.org
 module Lotus
+
+  # Return the current environment
+  # @since x.x.x
+  #
+  # @example Lotus.env => "development"
+
+  def self.env
+    Environment.new.environment
+  end
+
+  # Check to see if specified environment matches the current environment
+  # @since x.x.x
+  #
+  # @example Lotus.env?(:development) => true
+
+  def self.env?(*names)
+    Environment.new.environment?(*names)
+  end
 end
