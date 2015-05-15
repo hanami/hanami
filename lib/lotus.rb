74f74119c746ea1bbb71eb3cfd3178158e0b2927
Luca Guidi <me@lucaguidi.com>
2015-05-15 13:27:16 +0200

    [ci-skip] Cleanup per #216

--- lib/lotus.rb
+++ lib/lotus.rb
@@ -12,19 +12,43 @@ require 'lotus/environment'
 module Lotus
 
   # Return the current environment
-  # @since x.x.x
   #
-  # @example Lotus.env => "development"
-
+  # @return [String] the current environment
+  #
+  # @since 0.3.1
+  #
+  # @see Lotus::Environment#environment
+  #
+  # @example
+  #   Lotus.env => "development"
   def self.env
     Environment.new.environment
   end
 
-  # Check to see if specified environment matches the current environment
-  # @since x.x.x
+  # Check to see if specified environment(s) matches the current environment.
   #
-  # @example Lotus.env?(:development) => true
-
+  # If multiple names are given, it returns true, if at least one of them
+  # matches the current environment.
+  #
+  # @return [TrueClass,FalseClass] the result of the check
+  #
+  # @since 0.3.1
+  #
+  # @see Lotus.env
+  #
+  # @example Single name
+  #   puts ENV['LOTUS_ENV'] # => "development"
+  #
+  #   Lotus.env?(:development)  # => true
+  #   Lotus.env?('development') # => true
+  #
+  #   Lotus.env?(:production)   # => false
+  #
+  # @example Multiple names
+  #   puts ENV['LOTUS_ENV'] # => "development"
+  #
+  #   Lotus.env?(:development, :test)   # => true
+  #   Lotus.env?(:production, :staging) # => false
   def self.env?(*names)
     Environment.new.environment?(*names)
   end
