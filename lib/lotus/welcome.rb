edd719a9486bf0ffbfc3663af3c4dbdba95ac741
Luca Guidi <me@lucaguidi.com>
2015-06-19 18:14:11 +0200

    Welcome page enhancements

--- lib/lotus/welcome.rb
+++ lib/lotus/welcome.rb
@@ -1,14 +1,33 @@
+require 'erb'
 require 'pathname'
+require 'lotus/environment'
+require 'lotus/utils/string'
 
 module Lotus
   class Welcome
     def initialize(app)
       @root = Pathname.new(__dir__).join('templates').realpath
-      @body = [@root.join('welcome.html').read]
+      @body = [ERB.new(@root.join('welcome.html.erb').read).result(binding)]
     end
 
     def call(env)
       [200, {}, @body]
+    end
+
+    def application_name
+      " #{ app }" if container?
+    end
+
+    private
+
+    def container?
+      Environment.new.container?
+    end
+
+    def app
+      Utils::String.new(
+        Application.applications.first
+      ).namespace.downcase
     end
   end
 end
