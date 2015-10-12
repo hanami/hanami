0677e96950d93b741a56fe1bc603a623cbf3c4e2
Brenno Costa <brennolncosta@gmail.com>
2015-10-12 15:45:39 -0300

    Improve DatabaseConfig to support non-CLI
    - Improved commands/new/container_test.rb

--- test/fixtures/collaboration/apps/web/application.rb
+++ test/fixtures/collaboration/apps/web/application.rb
@@ -3,26 +3,30 @@ require 'lotus/model'
 require 'lotus/helpers'
 require 'securerandom'
 
-ADAPTER_TYPE =  if RUBY_ENGINE == 'jruby'
-                  require 'jdbc/sqlite3'
-                  Jdbc::SQLite3.load_driver
-                  'jdbc:sqlite'
-                else
-                  require 'sqlite3'
-                  'sqlite'
-                end
+ADAPTER_TYPE = if Lotus::Utils.jruby?
+                 require 'jdbc/sqlite3'
+                 Jdbc::SQLite3.load_driver
+
+                 'jdbc:sqlite'
+               else
+                 require 'sqlite3'
+
+                 'sqlite'
+               end
 
 require 'lotus/model/adapters/sql_adapter'
+
 db = Pathname.new(File.dirname(__FILE__)).join('../tmp/test.sqlite3')
 db.dirname.mkpath      # create directory if not exist
 db.delete if db.exist? # delete file if exist
-SQLITE_CONNECTION_STRING = "#{ADAPTER_TYPE}://#{ db }"
+
+SQLITE_CONNECTION_STRING = "#{ ADAPTER_TYPE }://#{ db }"
 
 DB = Sequel.connect(SQLITE_CONNECTION_STRING)
 
 DB.create_table :books do
   primary_key :id
-  String  :name
+  String :name
 end
 
 module Collaboration
@@ -30,7 +34,7 @@ module Collaboration
     configure do
       layout :application
       load_paths << 'app'
-      routes  'config/routes'
+      routes 'config/routes'
 
       serve_assets true
       sessions :cookie, secret: SecureRandom.hex
