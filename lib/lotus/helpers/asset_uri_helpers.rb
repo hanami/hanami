3dfcc84823a9fd185eb5e7f6251f9ffebbbf4828
Florian Breisch <florian.breisch@mindkeeper.solutions>
2015-11-27 16:58:28 +0100

    add skeleton for asset-uri-helpers

---
+++ lib/lotus/helpers/asset_uri_helpers.rb
@@ -0,0 +1,17 @@
+module Lotus
+  module Helpers
+    # Helper methods to generate asset-paths
+    #
+    # @since  0.6.0
+    # @api public
+    module AssetUriHelpers
+      # Generates the application-specific relative paths for assets
+      def asset_path(args)
+      end
+
+      # Generates the application-specific absolute URL for assets
+      def asset_url(args)
+      end
+    end
+  end
+end
