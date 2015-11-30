df4c07c26281b6ab519c05d03c56af76f2cc08ae
Luca Guidi <me@lucaguidi.com>
2015-11-30 17:59:23 +0100

    Use new SimpleCov syntax for MultiFormatter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,10 +8,10 @@ if ENV['COVERAGE'] == 'true'
   require 'simplecov'
   require 'coveralls'
 
-  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
     SimpleCov::Formatter::HTMLFormatter,
     Coveralls::SimpleCov::Formatter
-  ]
+  ])
 
   SimpleCov.start do
     command_name 'test'
