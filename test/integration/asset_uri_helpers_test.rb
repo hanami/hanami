703eaaeb02ea4cebaaf6a529ca20e7d9ba774e7d
Florian Breisch <florian.breisch@mindkeeper.solutions>
2015-12-02 16:12:42 +0100

    fix integration-test

--- test/integration/asset_uri_helpers_test.rb
+++ test/integration/asset_uri_helpers_test.rb
@@ -21,16 +21,16 @@ describe 'Collaboration::Application::View include AssetUriHelpers' do
     last_response
   end
 
-  describe Lotus::View do
+  describe 'Lotus::View' do
     it 'is able to access asset_path/_url in the view-object and template' do
       get "/use-uri-helpers"
 
       response.status.must_equal 200
 
-      response.body.must_include "template:/assets/application.jpg"
-      response.body.must_include "template:/assets/application.jpg"
-      response.body.must_include "view:/assets/application.jpg"
-      response.body.must_include "view:/assets/application.jpg"
+      response.body.must_include "template:&#x2F;assets&#x2F;application.jpg"
+      response.body.must_include "template:http:&#x2F;&#x2F;localhost:2300&#x2F;assets&#x2F;application.jpg"
+      response.body.must_include "view:&#x2F;assets&#x2F;application.jpg"
+      response.body.must_include "view:http:&#x2F;&#x2F;localhost:2300&#x2F;assets&#x2F;application.jpg"
     end
   end
 end
