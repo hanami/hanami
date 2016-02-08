7248d98b1abdac85a6c2bb7dc9fc97e665fce2d9
Anton Davydov <antondavydov.o@gmail.com>
2016-02-08 20:40:14 +0300

    Generate less strict hanami version on Gemfile

--- lib/hanami/version.rb
+++ lib/hanami/version.rb
@@ -1,6 +1,11 @@
 module Hanami
-  # Defines the version
+  # Defines the full version
   #
   # @since 0.1.0
   VERSION = '0.8.0'.freeze
+
+  # Defines the version without tiny part
+  #
+  # @since x.x.x
+  VERSION_WITHOUT_TINY = '0.8'.freeze
 end
