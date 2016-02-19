c69180e4b8d3a6691c3a0453fd1fa360280d812e
Anton Davydov <antondavydov.o@gmail.com>
2016-02-19 12:51:54 +0300

    Create option for set custom log instance instead Hanami::Logger

--- lib/hanami/config/logger.rb
+++ lib/hanami/config/logger.rb
@@ -32,6 +32,38 @@ module Hanami
         end
       end
 
+      # Custom log instance value. `nil` by default
+      #
+      # @overload stream(value)
+      #   Sets the given value
+      #   @param value [String] for custom log instance value.
+      #
+      # @overload stream
+      #   Gets the value
+      #   @return [String] custom log instance value
+      #
+      # @since x.x.x
+      # @api private
+      def custom_logger(value = nil)
+        if value.nil?
+          @custom_logger
+        else
+          @custom_logger = value
+        end
+      end
+
+      # Application name value.
+      #
+      # @overload stream(value)
+      #   Sets the given value
+      #   @param value [String] for app name option
+      #
+      # @overload stream
+      #   Gets the value
+      #   @return [String] app name option's value
+      #
+      # @since x.x.x
+      # @api private
       def app_name(value = nil)
         if value.nil?
           @app_name
@@ -47,7 +79,8 @@ module Hanami
       # @since x.x.x
       # @api private
       def build
-        ::Hanami::Logger.new(@app_name, device: @device)
+        @custom_logger ||
+          ::Hanami::Logger.new(@app_name, device: @device)
       end
     end
   end
--- test/config/logger_test.rb
+++ test/config/logger_test.rb
@@ -22,6 +22,19 @@ describe Hanami::Config::Logger do
     end
   end
 
+  describe '#custom logger' do
+    let(:custom_logger) { ::Logger.new(STDOUT) }
+
+    it 'contains name' do
+      logger.custom_logger(custom_logger)
+      logger.custom_logger.must_equal custom_logger
+    end
+
+    it 'contains nil by default' do
+      logger.custom_logger.must_equal nil
+    end
+  end
+
   describe '#build' do
     it 'returns new Utils::Logger instance' do
       logger.build.must_be_instance_of Hanami::Logger
@@ -36,6 +49,15 @@ describe Hanami::Config::Logger do
 
         builded_logger.must_be_instance_of Hanami::Logger
         builded_logger.instance_variable_get("@device").must_equal io
+      end
+    end
+
+    describe 'when user set custom logger instance' do
+      it 'returns new Utils::Logger instance with changed log_device' do
+        logger.custom_logger(::Logger.new(STDOUT))
+        builded_logger = logger.build
+
+        builded_logger.must_be_instance_of ::Logger
       end
     end
   end
