91bfb1d89b8c96ea76398319e29a4647298806c4
Luca Guidi <me@lucaguidi.com>
2016-08-10 09:39:40 +0200

    Skip three tests from "modularized" architecture, which is private architecture and we won't support it anymore
    Ref https://github.com/hanami/utils/pull/152

--- test/integration/modulized_application_test.rb
+++ test/integration/modulized_application_test.rb
@@ -24,13 +24,16 @@ describe 'A modulized Hanami application' do
     last_response
   end
 
-  it 'returns a successful response for a resource' do
-    get '/catalog'
+  # This test is skipped because we won't support "modularized" architecture anymore.
+  # See https://github.com/hanami/utils/pull/152
+  it 'returns a successful response for a resource'
+  # it 'returns a successful response for a resource' do
+  #   get '/catalog'
 
-    response.status.must_equal 200
-    response.body.must_match %(<title>Furnitures</title>)
-    response.body.must_match %(<h1>Catalog</h1>)
-  end
+  #   response.status.must_equal 200
+  #   response.body.must_match %(<title>Furnitures</title>)
+  #   response.body.must_match %(<h1>Catalog</h1>)
+  # end
 
   it "renders a custom page for not found resources" do
     get '/unknown'
@@ -49,23 +52,29 @@ describe 'A modulized Hanami application' do
     response.body.must_match %(<h2>500 - Internal Server Error</h2>)
   end
 
-  it "handles redirects from routes" do
-    get '/legacy'
-    follow_redirect!
+  # This test is skipped because we won't support "modularized" architecture anymore.
+  # See https://github.com/hanami/utils/pull/152
+  it "handles redirects from routes"
+  # it "handles redirects from routes" do
+  #   get '/legacy'
+  #   follow_redirect!
 
-    response.status.must_equal 200
-    response.body.must_match %(<title>Furnitures</title>)
-    response.body.must_match %(<h1>Catalog</h1>)
-  end
+  #   response.status.must_equal 200
+  #   response.body.must_match %(<title>Furnitures</title>)
+  #   response.body.must_match %(<h1>Catalog</h1>)
+  # end
 
-  it "handles redirects from actions" do
-    get '/action_legacy'
-    follow_redirect!
+  # This test is skipped because we won't support "modularized" architecture anymore.
+  # See https://github.com/hanami/utils/pull/152
+  it "handles redirects from actions"
+  # it "handles redirects from actions" do
+  #   get '/action_legacy'
+  #   follow_redirect!
 
-    response.status.must_equal 200
-    response.body.must_match %(<title>Furnitures</title>)
-    response.body.must_match %(<h1>Catalog</h1>)
-  end
+  #   response.status.must_equal 200
+  #   response.body.must_match %(<title>Furnitures</title>)
+  #   response.body.must_match %(<h1>Catalog</h1>)
+  # end
 
   it "handles thrown statuses from actions" do
     get '/protected'
