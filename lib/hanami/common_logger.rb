21b7f8930e7953a910458a8ece7b8efd966baf73
Anton Davydov <antondavydov.o@gmail.com>
2016-08-19 13:30:01 +0300

    Simple refactor common logger

--- lib/hanami/common_logger.rb
+++ lib/hanami/common_logger.rb
@@ -1,6 +1,7 @@
 module Hanami
   class CommonLogger < ::Rack::CommonLogger
     EMPTY_STRING = ''.freeze
+    ROOT_PATH = '/'.freeze
 
     def call(env)
       began_at = Time.now
@@ -13,31 +14,39 @@ module Hanami
   private
 
     def log(env, status, header, began_at)
-      now = Time.now
-
-      msg = {
-        addr: env['HTTP_X_FORWARDED_FOR'] || env['REMOTE_ADDR'],
-        now_time: now.strftime("'%d/%b/%Y %H:%M:%S %z'"),
-        request_method: env['REQUEST_METHOD'],
-        path: env['PATH_INFO'] + query_string(env),
-        http_version: env['HTTP_VERSION'],
-        length: extract_content_length(header),
-        status: status.to_s[0..3],
-        time: now - began_at
-      }
+      msg = generate_message(env, status, began_at)
+      length = extract_content_length(header)
+      msg[:length] = length if length
 
       # Standard library logger doesn't support write but it supports << which actually
       # calls to write on the log device without formatting
       @logger.log(@logger.level, msg)
     end
 
-    def query_string(env)
-      env['QUERY_STRING'].empty? ? EMPTY_STRING : "?#{env['QUERY_STRING']}"
+    def full_path(env)
+      path = env['PATH_INFO'].empty? ? ROOT_PATH : env['PATH_INFO']
+      query = env['QUERY_STRING'].empty? ? EMPTY_STRING : "?#{env['QUERY_STRING']}"
+
+      path + query
     end
 
     def extract_content_length(header)
-      value = header['Content-Length'] or return
+      value = header['Content-Length'] || return
       value.to_s == '0' ? nil : value
+    end
+
+    def generate_message(env, status, began_at)
+      now = Time.now
+
+      {
+        addr: env['HTTP_X_FORWARDED_FOR'] || env['REMOTE_ADDR'],
+        now_time: now.strftime("'%d/%b/%Y %H:%M:%S %z'"),
+        request_method: env['REQUEST_METHOD'],
+        path: full_path(env).inspect,
+        http_version: env['HTTP_VERSION'],
+        status: status.to_s[0..3],
+        time: now - began_at
+      }
     end
   end
 end
