4f47a370a34562cbe0f52fecda9c6502a3a2613c
Luca Guidi <me@lucaguidi.com>
2016-10-19 17:29:53 +0200

    Integration test for Subresource Integrity shouldn't rely on order of assets listed in manifest

--- spec/integration/assets/subresource_integrity_spec.rb
+++ spec/integration/assets/subresource_integrity_spec.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 RSpec.describe "assets", type: :cli do
   describe "subresource integrity" do
     it "precompiles assets with checksums calculated with given algorithms" do
@@ -32,9 +34,11 @@ EOF
 
         hanami "assets precompile"
 
-        expect("public/assets.json").to have_file_content <<-EOF
-{"/assets/application.css":{"target":"/assets/application-068e7d97b3671a14824bc20437ac5d06.css","sri":["sha256-cHgODQTP2nNk9ER+ViDGp+lC+ddHRmuLgJk05glJ4+w=","sha512-TDyxo1Ow7UjBib6ykALJh7J1OHEcE0yX4X21s1714ZBAhwdOz7k9t8+QQDAwWAmeH97bNaZGY7oTfVrwyTQ3cw=="]},"/assets/favicon.ico":{"target":"/assets/favicon-2d931609a81d94071c81890f77209101.ico","sri":["sha256-QxGPbQhTL64Lp6vYed7gabWjwB7Uhxkiztdj7LCU23A=","sha512-riH2RZwaTbMOmvfrUcpRWMQORhX9Bz9PLfENwDWm93wD+ESkaIUiYqDl+NShF0hja4L67lTbMglW8lUSmF3Tsg=="]}}
-EOF
+        manifest = File.read("public/assets.json")
+        expect(JSON.parse(manifest)).to be_kind_of(Hash) # assert it's a well-formed JSON
+
+        expect(manifest).to include(%("/assets/application.css":{"target":"/assets/application-068e7d97b3671a14824bc20437ac5d06.css","sri":["sha256-cHgODQTP2nNk9ER+ViDGp+lC+ddHRmuLgJk05glJ4+w=","sha512-TDyxo1Ow7UjBib6ykALJh7J1OHEcE0yX4X21s1714ZBAhwdOz7k9t8+QQDAwWAmeH97bNaZGY7oTfVrwyTQ3cw=="]}))
+        expect(manifest).to include(%("/assets/favicon.ico":{"target":"/assets/favicon-2d931609a81d94071c81890f77209101.ico","sri":["sha256-QxGPbQhTL64Lp6vYed7gabWjwB7Uhxkiztdj7LCU23A=","sha512-riH2RZwaTbMOmvfrUcpRWMQORhX9Bz9PLfENwDWm93wD+ESkaIUiYqDl+NShF0hja4L67lTbMglW8lUSmF3Tsg=="]}))
 
         server do
           visit '/'
