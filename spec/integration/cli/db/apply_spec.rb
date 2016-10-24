f003e8600f23b99366dd8f4d99b784e05560a837
Luca Guidi <me@lucaguidi.com>
2016-10-24 12:25:47 +0200

    Restore CLI integration tests for `hanami db apply`

--- spec/integration/cli/db/apply_spec.rb
+++ spec/integration/cli/db/apply_spec.rb
@@ -12,17 +12,15 @@ RSpec.describe "hanami db", type: :cli do
         expect(out).to include(versions.last.to_s)
 
         db         = Pathname.new('db')
-        _schema    = db.join('schema.sql').to_s
+        schema     = db.join('schema.sql').to_s
         migrations = db.join('migrations')
 
-# rubocop:disable Style/CommentIndentation
-#       expect(schema).to have_file_content <<-SQL
-# CREATE TABLE `schema_migrations` (`filename` varchar(255) NOT NULL PRIMARY KEY);
-# CREATE TABLE `users` (`id` integer NOT NULL PRIMARY KEY AUTOINCREMENT, `name` varchar(255), `age` integer);
-# INSERT INTO "schema_migrations" VALUES('#{versions.first}_create_users.rb');
-# INSERT INTO "schema_migrations" VALUES('#{versions.last}_add_age_to_users.rb');
-# SQL
-# rubocop:enable Style/CommentIndentation
+        expect(schema).to have_file_content <<-SQL
+CREATE TABLE `schema_migrations` (`filename` varchar(255) NOT NULL PRIMARY KEY);
+CREATE TABLE `users` (`id` integer NOT NULL PRIMARY KEY AUTOINCREMENT, `name` varchar(255), `age` integer);
+INSERT INTO "schema_migrations" VALUES('#{versions.first}_create_users.rb');
+INSERT INTO "schema_migrations" VALUES('#{versions.last}_add_age_to_users.rb');
+SQL
 
         expect(migrations.children).to be_empty
       end
