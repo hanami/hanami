ce9bce1f93d07253ec2865c69ac6c1f2b913679c
Luca Guidi <me@lucaguidi.com>
2016-12-06 17:11:01 +0100

    Integration specs for sessions

--- spec/integration/sessions_spec.rb
+++ spec/integration/sessions_spec.rb
@@ -1,55 +1,65 @@
 RSpec.describe "Sessions", type: :cli do
-  let(:token) { "abc123" }
+  it "shows welcome page" do
+    with_project do
+      server do
+        visit "/"
+
+        expect(page).to have_title("Hanami | The web, with simplicity")
+
+        expect(page).to have_content("The web, with simplicity.")
+        expect(page).to have_content("Hanami is Open Source Software for MVC web development with Ruby.")
+        expect(page).to have_content("bundle exec hanami generate action web home#index --url=/")
+      end
+    end
+  end
 
   it "is empty by default" do
     with_project do
       prepare
 
       server do
-        get '/session'
+        visit '/'
 
-        expect(last_response.status).to eq(200)
-        expect(last_response.body).to   eq("[empty]")
+        expect(current_path).to eq('/')
+        expect(page).to         have_content('Sign in')
       end
     end
   end
 
-  xit "preserves data across requests" do
+  it "preserves data across requests" do
     with_project do
       prepare
 
       server do
-        # Create
-        post '/session', name: "Luca", _csrf_token: token
+        visit '/'
+        expect(current_path).to eq('/')
 
-        expect(last_response.status).to eq(200)
-        expect(last_response.body).to   eq("Session created for: Luca")
+        click_link('Sign in')
+        expect(current_path).to eq('/signin')
 
-        # Read
-        get '/session', nil, 'HTTP_COOKIE' => last_response.headers['Set-Cookie']
+        within 'form#signin-form' do
+          click_button 'Sign in'
+        end
 
-        expect(last_response.status).to eq(200)
-        expect(last_response.body).to   eq("Luca")
+        visit '/' # Without this, Capybara losts the session.
+
+        expect(current_path).to eq('/')
+        expect(page).to have_content("Welcome, Luca")
       end
     end
   end
 
-  xit "clears the session" do
+  it "clears the session" do
     with_project do
       prepare
 
       server do
-        # Create
-        post '/session', name: "Luca", _csrf_token: token
+        given_signedin_user
 
-        expect(last_response.status).to eq(200)
-        expect(last_response.body).to   eq("Session created for: Luca")
+        click_link "Sign out"
 
-        # Delete
-        delete '/session', { _csrf_token: token }, 'HTTP_COOKIE' => last_response.headers['Set-Cookie']
-
-        expect(last_response.status).to eq(200)
-        expect(last_response.body).to   eq("Session cleared for: Luca")
+        expect(current_path).to eq('/')
+        expect(page).to_not have_content("Welcome, Luca")
       end
     end
   end
@@ -60,41 +70,101 @@ RSpec.describe "Sessions", type: :cli do
     # Enable sessions
     replace "apps/web/application.rb", "# sessions :cookie, secret: ENV['WEB_SESSIONS_SECRET']", "sessions :cookie, secret: ENV['WEB_SESSIONS_SECRET']"
 
-    # Make CSRF token protection to always return "token" in order to make testing easier
-    replace "apps/web/application.rb", "controller.prepare do", "controller.prepare do\ninclude Module.new {\ndef generate_csrf_token\n'#{token}'\nend\n}"
-
+    generate_user
     generate_actions
   end
 
-  def generate_actions # rubocop:disable Metrics/MethodLength
-    generate "action web sessions#create --url=/session --method=POST"
-    generate "action web sessions#show --url=/session --method=GET"
-    generate "action web sessions#destroy --url=/session --method=DELETE"
+  def generate_user # rubocop:disable Metrics/MethodLength
+    generate_model     "user"
+    generate_migration "create_users", <<-EOF
+Hanami::Model.migration do
+  change do
+    create_table :users do
+      primary_key :id
+      column :name, String
+    end
 
+    execute "INSERT INTO users (name) VALUES('Luca')"
+  end
+end
+EOF
+
+    hanami "db prepare"
+  end
+
+  def generate_actions
+    generate_home
+    generate_signin
+    generate_signout
+  end
+
+  def generate_home # rubocop:disable Metrics/MethodLength
+    generate "action web home#index --url=/"
+    replace "apps/web/config/routes.rb", "home#index", 'root to: "home#index"'
+
+    rewrite "apps/web/controllers/home/index.rb", <<-EOF
+module Web::Controllers::Home
+  class Index
+    include Web::Action
+    expose :current_user
+
+    def call(params)
+      @current_user = UserRepository.new.find(session[:user_id])
+    end
+  end
+end
+EOF
+
+    rewrite "apps/web/templates/home/index.html.erb", <<-EOF
+<h1>Bookshelf</h1>
+
+<% if current_user.nil? %>
+  <%= link_to "Sign in", "/signin" %>
+<% else %>
+  Welcome, <%= current_user.name %>
+  <%= link_to "Sign out", "/signout" %>
+<% end %>
+EOF
+  end
+
+  def generate_signin
+    generate_signin_new_action
+    generate_signin_create_action
+  end
+
+  def generate_signin_new_action
+    generate "action web sessions#new --url=/signin --method=GET"
+    rewrite "apps/web/templates/sessions/new.html.erb", <<-EOF
+<h1>Sign in</h1>
+
+<%=
+  form_for :signin, "/signin", id: "signin-form" do
+    div do
+      button "Sign in"
+    end
+  end
+%>
+EOF
+  end
+
+  def generate_signin_create_action
+    generate "action web sessions#create --url=/signin --method=POST"
     rewrite "apps/web/controllers/sessions/create.rb", <<-EOF
 module Web::Controllers::Sessions
   class Create
     include Web::Action
 
     def call(params)
-      session[:name] = params[:name]
-      self.body = "Session created for: \#{session[:name]}"
+      session[:user_id] = UserRepository.new.first.id
+      redirect_to routes.root_url
     end
   end
 end
 EOF
-
-    rewrite "apps/web/controllers/sessions/show.rb", <<-EOF
-module Web::Controllers::Sessions
-  class Show
-    include Web::Action
-
-    def call(params)
-      self.body = session[:name] || "[empty]"
-    end
   end
-end
-EOF
+
+  def generate_signout
+    generate "action web sessions#destroy --url=/signout --method=GET"
 
     rewrite "apps/web/controllers/sessions/destroy.rb", <<-EOF
 module Web::Controllers::Sessions
@@ -102,12 +172,28 @@ module Web::Controllers::Sessions
     include Web::Action
 
     def call(params)
-      name = session[:name]
-      session.clear
-      self.body = "Session cleared for: \#{name}"
+      session[:user_id] = nil
+      redirect_to routes.root_url
     end
   end
 end
 EOF
+  end
+
+  def given_signedin_user # rubocop:disable Metrics/AbcSize
+    visit '/'
+    expect(current_path).to eq('/')
+
+    click_link('Sign in')
+    expect(current_path).to eq('/signin')
+
+    within 'form#signin-form' do
+      click_button 'Sign in'
+    end
+
+    visit '/' # Without this, Capybara losts the session.
+
+    expect(current_path).to eq('/')
+    expect(page).to have_content("Welcome, Luca")
   end
 end
