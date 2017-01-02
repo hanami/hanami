6e80d339e97b67563c75daa5b2b6cf4d4bceffd8
Marcello Rocha <mereghost@users.noreply.github.com>
2017-01-02 15:34:51 -0200

    Adds the missing AUTOINCREMENT keyword to migrate_spec (#708)

--- spec/integration/cli/db/migrate_spec.rb
+++ spec/integration/cli/db/migrate_spec.rb
@@ -51,7 +51,7 @@ RSpec.describe "hanami db", type: :cli do
         db = Pathname.new("db").join("#{project}_development.sqlite")
 
         users = `sqlite3 #{db} ".schema users"`
-        expect(users).to include("CREATE TABLE `users`(`id` integer DEFAULT (NULL) NOT NULL PRIMARY KEY, `name` varchar(255) DEFAULT (NULL) NULL);")
+        expect(users).to include("CREATE TABLE `users`(`id` integer DEFAULT (NULL) NOT NULL PRIMARY KEY AUTOINCREMENT, `name` varchar(255) DEFAULT (NULL) NULL);")
 
         version = `sqlite3 #{db} "SELECT filename FROM schema_migrations ORDER BY filename DESC LIMIT 1"`
         expect(version).to include("create_users")
