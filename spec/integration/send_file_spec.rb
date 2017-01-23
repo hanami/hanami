2203c1c4443b48c0fe0a101825c26c546a065753
Luca Guidi <me@lucaguidi.com>
2017-01-23 10:27:49 +0100

    Make Hanami compatible with Rack 2 (#711)

--- spec/integration/send_file_spec.rb
+++ spec/integration/send_file_spec.rb
@@ -1,6 +1,31 @@
 RSpec.describe "Send file", type: :cli do
   it "sends file" do
     with_project do
+      write "public/static.txt", "Static file"
+      generate "action web home#index --url=/"
+      rewrite "apps/web/controllers/home/index.rb", <<-EOF
+module Web::Controllers::Home
+  class Index
+    include Web::Action
+
+    def call(params)
+      send_file "static.txt"
+    end
+  end
+end
+EOF
+
+      server do
+        get '/'
+
+        expect(last_response.status).to eq(200)
+        expect(last_response.body).to   include("Static file")
+      end
+    end
+  end
+
+  it "doesn't send file outside of public directory" do
+    with_project do
       generate "action web home#index --url=/"
       rewrite "apps/web/controllers/home/index.rb", <<-EOF
 module Web::Controllers::Home
@@ -17,8 +42,7 @@ EOF
       server do
         get '/'
 
-        expect(last_response.status).to eq(200)
-        expect(last_response.body).to   include("Web::Controllers::Home")
+        expect(last_response.status).to eq(404)
       end
     end
   end
