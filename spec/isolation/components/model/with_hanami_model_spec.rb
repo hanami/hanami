16d14341ad6e12145c9533ebcb7de11e99d47748
Luca Guidi <me@lucaguidi.com>
2017-03-13 12:27:20 +0100

    Disconnect database during the boot process. (#748)
    * Disconnect database during the boot process.
    This is useful to restart projects in production, so the system can prune stale database connections.
    
    * Use hanami-model stable branch
    
    * Typo [ci skip]

--- spec/isolation/components/model/with_hanami_model_spec.rb
+++ spec/isolation/components/model/with_hanami_model_spec.rb
@@ -10,5 +10,38 @@ RSpec.describe "Components: model", type: :cli do
         expect(Hanami::Model.configuration.logger).to eq(Hanami.logger)
       end
     end
+
+    it "disconnects database connections on reboot" do
+      with_project do
+        require Pathname.new(Dir.pwd).join("config", "environment")
+
+        # Simulate previous connection
+        Hanami::Components.resolve('model.configuration')
+        Hanami::Components.resolve('model.sql')
+        Hanami::Model.load!
+
+        #
+        Hanami::Model.configuration.connection.test_connection
+        # Current connections count is 1
+
+        #
+        Hanami::Components.resolve('model')
+        # Current connections count is 0
+
+        expect(Hanami::Components['model']).to     be(true)
+        expect(Hanami::Components['model.sql']).to be(true)
+        expect(Hanami::Model.configuration.logger).to eq(Hanami.logger)
+
+        # This is tight coupled to Sequel
+        #
+        # When `.disconnect` is invoked, it returns a collection of disconnected
+        # connections. Here we want to assert that `.disconnect` was invoked as
+        # part of the boot process.
+        #
+        # Invoking disconnect again **here in the test** should return an empty
+        # collection, because we haven't tried to connect to the database again.
+        expect(Hanami::Model.disconnect).to be_empty
+      end
+    end
   end
 end
