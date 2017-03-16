074657df678c49261d538dd4c9b861d36e2f2d92
Robert robbl Schulze <robert.schulze@aboutsource.net>
2017-03-16 11:16:12 +0100

    Add spec for Hanami::Configuration::App with optional host argument

--- spec/isolation/hanami/configure_spec.rb
+++ spec/isolation/hanami/configure_spec.rb
@@ -1,17 +1,20 @@
 RSpec.describe "Hanami.configure" do
   before do
-    application   = app
-    model_config  = model_configuration
-    mailer_config = mailer_configuration
+    application           = app
+    application_with_host = app_with_host
+    model_config          = model_configuration
+    mailer_config         = mailer_configuration
 
     Hanami.configure do
       mount application, at: '/'
+      mount application_with_host, at: '/', host: 'hanami'
       model(&model_config)
       mailer(&mailer_config)
     end
   end
 
   let(:app) { double('app') }
+  let(:app_with_host) { double('app') }
 
   let(:model_configuration) do
     lambda do
@@ -34,6 +37,7 @@ RSpec.describe "Hanami.configure" do
   it "setups apps" do
     mounted = Hanami.configuration.mounted
     expect(mounted[app]).to eq(Hanami::Configuration::App.new(app, '/'))
+    expect(mounted[app_with_host]).to eq(Hanami::Configuration::App.new(app_with_host, '/', 'hanami'))
   end
 
   it "holds model configuration" do
