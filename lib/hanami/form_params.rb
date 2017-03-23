bd7a5cce0092bbc7429bd05d8f9b33b285add3b0
Gabriel Gizotti <gabriel@gizotti.com>
2017-03-23 22:44:23 +1000

    Add form param filtering and ability to customise filters for hash params

--- lib/hanami/form_params.rb
+++ lib/hanami/form_params.rb
@@ -6,8 +6,9 @@ module Hanami
   # @since x.x.x
   # @api private
   class FormParams
-    def initialize(params)
-      @params = params && Hanami::Utils::Hash.new(params).deep_symbolize!
+    def initialize(params, filtered_parameters: Hanami.configuration.filtered_parameters)
+      @params = params
+      @filtered_parameters = filtered_parameters
     end
 
     # @since x.x.x
@@ -15,7 +16,7 @@ module Hanami
     def prepared_params
       return unless present?
 
-      @prepared_params ||= JSON.pretty_generate(@params.to_h)
+      @prepared_params ||= JSON.pretty_generate(filtered_params.deep_symbolize!.to_h)
     end
 
     # @since x.x.x
@@ -30,6 +31,44 @@ module Hanami
     # @api private
     def present?
       !@params.nil?
+    end
+
+    private
+
+    def filtered_params
+      filtered_params = if @filtered_parameters.nil?
+                          @params
+                        else
+                          filter_hash(@params)
+                        end
+
+      Hanami::Utils::Hash.new(filtered_params)
+    end
+
+    def filter_hash(hash)
+      Hash[
+        hash.map do |k, v|
+          if filtered_key?(k)
+            [k, '[FILTERED]']
+          else
+            [k, process_hash_value(v)]
+          end
+        end
+      ]
+    end
+
+    def process_hash_value(v)
+      return v unless v.is_a?(Hash)
+
+      filter_hash(v)
+    end
+
+    def filtered_key?(key)
+      @filtered_parameters.any? do |filter|
+        processed_filter = filter.is_a?(Symbol) ? filter.to_s : filter
+
+        key.match?(processed_filter)
+      end
     end
   end
 end
--- spec/unit/form_params_spec.rb
+++ spec/unit/form_params_spec.rb
@@ -1,15 +1,40 @@
 RSpec.describe Hanami::FormParams do
+  let(:filtered_parameters) { [/.*password.*/, :credit_card] }
   let(:raw_params) do
     Hash[
       'post' => Hash[
         'title' => 'My Post',
-        'body' => 'Blog post body'
+        'body' => 'Blog post body',
+        'credit_card' => {
+          'number' => '123123123123123',
+          'name' => 'Gabriel Gizotti'
+        },
+        'user' => {
+          'name' => 'User',
+          'password' => 'password1234'
+        },
+        'password' => 'password',
+        'password_confirmation' => 'password'
       ]
     ]
   end
-  let(:pretty_generated_params) { "{\n" + "  \"post\": {\n" + "    \"title\": \"My Post\",\n" + "    \"body\": \"Blog post body\"\n" + "  }\n" + "}" }
-  let(:present_params) { described_class.new(raw_params) }
-  let(:absent_params) { described_class.new(nil) }
+  let(:pretty_generated_params) do
+    "{\n" \
+    "  \"post\": {\n" \
+    "    \"title\": \"My Post\",\n" \
+    "    \"body\": \"Blog post body\",\n" \
+    "    \"credit_card\": \"[FILTERED]\",\n" \
+    "    \"user\": {\n" \
+    "      \"name\": \"User\",\n" \
+    "      \"password\": \"[FILTERED]\"\n" \
+    "    },\n" \
+    "    \"password\": \"[FILTERED]\",\n" \
+    "    \"password_confirmation\": \"[FILTERED]\"\n" \
+    "  }\n" \
+    "}"
+  end
+  let(:present_params) { described_class.new(raw_params, filtered_parameters: filtered_parameters) }
+  let(:absent_params) { described_class.new(nil, filtered_parameters: filtered_parameters) }
 
   describe '#prepared_params' do
     it 'return pretty printed params hash if initiated with params hash' do
