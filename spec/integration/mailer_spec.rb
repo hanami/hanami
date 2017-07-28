663c33ff7013dc31b259fdf53a07ffe745d46448
Alfonso Uceda Pompa <alfonso@bebanjo.com>
2017-07-28 17:09:28 +0200

    Fix prepare block for mailers

---
+++ spec/integration/mailer_spec.rb
@@ -0,0 +1,30 @@
+RSpec.describe "Mailer", type: :cli do
+  it "use a mailer" do
+    with_project do
+      generate "mailer welcome"
+      write "lib/bookshelf/mailers/default_user.rb", <<-EOF
+module Mailers
+  module DefaultUser
+    def user_name
+      "Alfonso"
+    end
+  end
+end
+EOF
+
+      replace "config/environment.rb", "delivery :test", <<-EOF
+    delivery :test
+
+    prepare do
+      include Mailers::DefaultUser
+    end
+EOF
+
+      console do |input, _, _|
+        input.puts("Mailers::Welcome.new.user_name")
+      end
+
+      expect(out).to include("Alfonso")
+    end
+  end
+end
