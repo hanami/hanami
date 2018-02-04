da9c152982914a0e702a668a4ad6cc0e91ad8173
Luca Guidi <me@lucaguidi.com>
2018-02-04 18:38:31 +0100

    Remove unnecessary isolation spec

--- spec/isolation/hanami/plugin/without_loaded_configuration_spec.rb
+++
@@ -1,5 +0,0 @@
-RSpec.describe "Hanami.plugin" do
-  it "raises error when try to confiure plugin before Hanami project" do
-    expect { Hanami.plugin{} }.to raise_error(RuntimeError, "Hanami not configured")
-  end
-end
