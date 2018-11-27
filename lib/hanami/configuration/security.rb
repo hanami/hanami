ad8551c0aa05b6164a62186992ba48f3a6379463
Luca Guidi <me@lucaguidi.com>
2018-11-27 11:39:16 +0100

    Enhanced security settings to make them serializable into HTTP headers

--- lib/hanami/configuration/security.rb
+++ lib/hanami/configuration/security.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require "concurrent/hash"
+require "hanami/utils/string"
 
 module Hanami
   class Configuration
@@ -10,14 +11,14 @@ module Hanami
     class Security
       def initialize
         @settings = Concurrent::Hash.new
-        self.x_frame_options = DEFAULT_X_FRAME_OPTIONS
-        self.x_content_type_options = DEFAULT_X_CONTENT_TYPE_OPTIONS
-        self.x_xss_protection = DEFAULT_X_XSS_PROTECTION
-        self.content_security_policy = DEFAULT_CONTENT_SECURITY_POLICY.dup
+        settings[:x_frame_options]         = Setting.new(HEADER_X_FRAME_OPTIONS, DEFAULT_X_FRAME_OPTIONS)
+        settings[:x_content_type_options]  = Setting.new(HEADER_X_CONTENT_TYPE_OPTIONS, DEFAULT_X_CONTENT_TYPE_OPTIONS)
+        settings[:x_xss_protection]        = Setting.new(HEADER_X_XSS_PROTECTION, DEFAULT_X_XSS_PROTECTION)
+        settings[:content_security_policy] = ContentSecurityPolicy.new(HEADER_CONTENT_SECURITY_POLICY, DEFAULT_CONTENT_SECURITY_POLICY.dup)
       end
 
       def x_frame_options=(value)
-        settings[:x_frame_options] = value
+        settings[:x_frame_options].value = value
       end
 
       def x_frame_options
@@ -25,7 +26,7 @@ module Hanami
       end
 
       def x_content_type_options=(value)
-        settings[:x_content_type_options] = value
+        settings[:x_content_type_options].value = value
       end
 
       def x_content_type_options
@@ -33,7 +34,7 @@ module Hanami
       end
 
       def x_xss_protection=(value)
-        settings[:x_xss_protection] = value
+        settings[:x_xss_protection].value = value
       end
 
       def x_xss_protection
@@ -41,25 +42,47 @@ module Hanami
       end
 
       def content_security_policy=(options)
-        settings[:content_security_policy] = options
+        settings[:content_security_policy].value = options
       end
 
       def content_security_policy
         settings.fetch(:content_security_policy)
       end
 
+      def to_hash
+        settings.each_with_object({}) do |(_, v), result|
+          next if v.value.nil?
+
+          result[v.header] = v.value
+        end
+      end
+
       private
 
-      attr_reader :settings
+      # X-Frame-Options
+      HEADER_X_FRAME_OPTIONS = "X-Frame-Options"
+      private_constant :HEADER_X_FRAME_OPTIONS
 
       DEFAULT_X_FRAME_OPTIONS = "DENY"
       private_constant :DEFAULT_X_FRAME_OPTIONS
 
+      # X-Content-Type-Optionx
+      HEADER_X_CONTENT_TYPE_OPTIONS = "X-Content-Type-Options"
+      private_constant :HEADER_X_CONTENT_TYPE_OPTIONS
+
       DEFAULT_X_CONTENT_TYPE_OPTIONS = "nosniff"
       private_constant :DEFAULT_X_CONTENT_TYPE_OPTIONS
 
+      # X-XSS-Protection
+      HEADER_X_XSS_PROTECTION = "X-XSS-Protection"
+      private_constant :HEADER_X_XSS_PROTECTION
+
       DEFAULT_X_XSS_PROTECTION = "1; mode=block"
       private_constant :DEFAULT_X_XSS_PROTECTION
+
+      # Content-Security-Policy
+      HEADER_CONTENT_SECURITY_POLICY = "Content-Security-Policy"
+      private_constant :HEADER_CONTENT_SECURITY_POLICY
 
       DEFAULT_CONTENT_SECURITY_POLICY = {
         form_action: "'self'",
@@ -78,6 +101,41 @@ module Hanami
         media_src: "'self'"
       }.freeze
       private_constant :DEFAULT_CONTENT_SECURITY_POLICY
+
+      # Security setting
+      #
+      # @api private
+      # @since 2.0.0
+      class Setting
+        attr_accessor :header, :value
+
+        def initialize(header, value)
+          @header = header.freeze
+          @value = value
+        end
+      end
+
+      # Content security policy settings
+      #
+      # @api private
+      # @since 2.0.0
+      class ContentSecurityPolicy < Setting
+        def value
+          @value.map do |k, v|
+            "#{Utils::String.dasherize(k)} #{v}" unless v.nil?
+          end.compact.join("; ")
+        end
+
+        def [](key)
+          @value[key]
+        end
+
+        def []=(key, value)
+          @value[key] = value
+        end
+      end
+
+      attr_reader :settings
     end
   end
 end
--- spec/unit/hanami/application/config/security_spec.rb
+++ spec/unit/hanami/application/config/security_spec.rb
@@ -7,25 +7,22 @@ RSpec.describe Hanami::Application do
       subject = app.config.security
 
       expect(subject).to be_kind_of(Hanami::Configuration::Security)
-      expect(subject.x_frame_options).to eq("DENY")
-      expect(subject.x_content_type_options).to eq("nosniff")
-      expect(subject.x_xss_protection).to eq("1; mode=block")
-      expect(subject.content_security_policy).to eq(
-        form_action: "'self'",
-        frame_ancestors: "'self'",
-        base_uri: "'self'",
-        default_src: "'none'",
-        script_src: "'self'",
-        connect_src: "'self'",
-        img_src: "'self' https: data:",
-        style_src: "'self' 'unsafe-inline' https:",
-        font_src: "'self'",
-        object_src: "'none'",
-        plugin_types: "application/pdf",
-        child_src: "'self'",
-        frame_src: "'self'",
-        media_src: "'self'"
-      )
+
+      expect(subject.x_frame_options).to be_kind_of(Hanami::Configuration::Security::Setting)
+      expect(subject.x_frame_options.header).to eq("X-Frame-Options")
+      expect(subject.x_frame_options.value).to eq("DENY")
+
+      expect(subject.x_content_type_options).to be_kind_of(Hanami::Configuration::Security::Setting)
+      expect(subject.x_content_type_options.header).to eq("X-Content-Type-Options")
+      expect(subject.x_content_type_options.value).to eq("nosniff")
+
+      expect(subject.x_xss_protection).to be_kind_of(Hanami::Configuration::Security::Setting)
+      expect(subject.x_xss_protection.header).to eq("X-XSS-Protection")
+      expect(subject.x_xss_protection.value).to eq("1; mode=block")
+
+      expect(subject.content_security_policy).to be_kind_of(Hanami::Configuration::Security::Setting)
+      expect(subject.content_security_policy.header).to eq("Content-Security-Policy")
+      expect(subject.content_security_policy.value).to eq("form-action 'self'; frame-ancestors 'self'; base-uri 'self'; default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self' https: data:; style-src 'self' 'unsafe-inline' https:; font-src 'self'; object-src 'none'; plugin-types application/pdf; child-src 'self'; frame-src 'self'; media-src 'self'")
     end
 
     it "returns set value" do
@@ -39,11 +36,24 @@ RSpec.describe Hanami::Application do
       subject = app.config.security
 
       expect(subject).to be_kind_of(Hanami::Configuration::Security)
-      expect(subject.x_frame_options).to eq("sameorigin")
-      expect(subject.x_content_type_options).to be(nil)
-      expect(subject.x_xss_protection).to eq("1")
+
+      expect(subject.x_frame_options).to be_kind_of(Hanami::Configuration::Security::Setting)
+      expect(subject.x_frame_options.header).to eq("X-Frame-Options")
+      expect(subject.x_frame_options.value).to eq("sameorigin")
+
+      expect(subject.x_content_type_options).to be_kind_of(Hanami::Configuration::Security::Setting)
+      expect(subject.x_content_type_options.header).to eq("X-Content-Type-Options")
+      expect(subject.x_content_type_options.value).to be(nil)
+
+      expect(subject.x_xss_protection).to be_kind_of(Hanami::Configuration::Security::Setting)
+      expect(subject.x_xss_protection.header).to eq("X-XSS-Protection")
+      expect(subject.x_xss_protection.value).to eq("1")
+
+      expect(subject.content_security_policy).to be_kind_of(Hanami::Configuration::Security::Setting)
       expect(subject.content_security_policy[:style_src]).to eq("'self' 'unsafe-inline' https: https://my.cdn.example")
       expect(subject.content_security_policy[:plugin_types]).to be(nil)
+      expect(subject.content_security_policy.header).to eq("Content-Security-Policy")
+      expect(subject.content_security_policy.value).to eq("form-action 'self'; frame-ancestors 'self'; base-uri 'self'; default-src 'none'; script-src 'self'; connect-src 'self'; img-src 'self' https: data:; style-src 'self' 'unsafe-inline' https: https://my.cdn.example; font-src 'self'; object-src 'none'; child-src 'self'; frame-src 'self'; media-src 'self'")
     end
   end
 end
