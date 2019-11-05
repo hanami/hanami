b13ddb25c0946b1b1692de074361cbb8a685c83b
Tim Riley <tim@icelab.com.au>
2019-11-05 21:38:23 +1100

    Fix typo
    Co-Authored-By: Piotr Solnica <piotr.solnica@gmail.com>

--- lib/hanami/components.rb
+++ lib/hanami/components.rb
@@ -4,4 +4,4 @@ require "dry/system"
 require "pathname"
 
 # FIXME: dry-system wants this to be a Pathname but it really shoudn't need to be
-Dry::System.register_provider :hamami, boot_path: Pathname(File.join(__dir__, "components"))
+Dry::System.register_provider :hanami, boot_path: Pathname(File.join(__dir__, "components"))
