8331078c0797ee5105c7ef2ac6eb2836509b42f2
Tim Riley <tim@openmonkey.com>
2019-11-07 22:10:50 +1100

    Remove dry-system components provider setup (we don't need it atm)

--- lib/hanami/components.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-require "dry/system"
-require "pathname"
-
-# FIXME: dry-system wants this to be a Pathname but it really shoudn't need to be
-Dry::System.register_provider :hanami, boot_path: Pathname(File.join(__dir__, "components"))
