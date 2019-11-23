d8aedb7bed9594953d0bc2a89d22c5b8f1ce9aff
Tim Riley <tim@openmonkey.com>
2019-11-23 22:34:43 +1100

    Remove references to as yet unimplemented CLI commands

--- lib/hanami/cli/application/commands.rb
+++ lib/hanami/cli/application/commands.rb
@@ -8,21 +8,7 @@ module Hanami
       module Commands
         extend Hanami::CLI::Registry
 
-        # require_relative "commands/assets/clobber"
-        # require_relative "commands/assets/compile"
         require_relative "commands/console"
-        # require_relative "commands/generate/slice"
-        # require_relative "commands/db/create"
-        # require_relative "commands/db/create_migration"
-        # require_relative "commands/db/drop"
-        # require_relative "commands/db/migrate"
-        # require_relative "commands/db/reset"
-        # require_relative "commands/db/rollback"
-        # require_relative "commands/db/sample_data"
-        # require_relative "commands/db/seed"
-        # require_relative "commands/db/structure/dump"
-        # require_relative "commands/db/version"
-        # require_relative "commands/routes/update"
       end
     end
   end
