38cae9b96dd1472f3a2519c190f7273032b20416
Tim Riley <tim@openmonkey.com>
2020-04-13 21:12:27 +1000

    Don’t try and turn nil into a Pathname

--- lib/hanami/slice.rb
+++ lib/hanami/slice.rb
@@ -14,7 +14,7 @@ module Hanami
       @application = application
       @name = name.to_sym
       @namespace = namespace
-      @root = Pathname(root)
+      @root = root ? Pathname(root) : root
       @container = container || define_container
     end
 
