20924a5c19a8d690513cdb50592a21cb33a3a351
Anton Davydov <antondavydov.o@gmail.com>
2020-04-27 17:55:39 +0300

    Introduce hanami version CLI command

--- spec/integration/cli/version_spec.rb
+++ spec/integration/cli/version_spec.rb
@@ -4,25 +4,25 @@ RSpec.describe "hanami version", type: :integration do
   context "within a project" do
     it "prints current version" do
       with_project do
-        run_command "hanami version", "v#{Hanami::VERSION}"
+        run_cmd "hanami version", "v#{Hanami::VERSION}"
       end
     end
 
     it "prints current version with v alias" do
       with_project do
-        run_command "hanami v", "v#{Hanami::VERSION}"
+        run_cmd "hanami v", "v#{Hanami::VERSION}"
       end
     end
 
     it "prints current version with -v alias" do
       with_project do
-        run_command "hanami -v", "v#{Hanami::VERSION}"
+        run_cmd "hanami -v", "v#{Hanami::VERSION}"
       end
     end
 
     it "prints current version with --version alias" do
       with_project do
-        run_command "hanami --version", "v#{Hanami::VERSION}"
+        run_cmd "hanami --version", "v#{Hanami::VERSION}"
       end
     end
 
@@ -42,26 +42,26 @@ RSpec.describe "hanami version", type: :integration do
             --help, -h                      	# Print this help
         OUT
 
-        run_command "hanami version --help", output
+        run_cmd "hanami version --help", output
       end
     end
   end
 
   context "outside of a project" do
     it "prints current version" do
-      run_command "hanami version", "v#{Hanami::VERSION}"
+      run_cmd "hanami version", "v#{Hanami::VERSION}"
     end
 
     it "prints current version with v alias" do
-      run_command "hanami v", "v#{Hanami::VERSION}"
+      run_cmd "hanami v", "v#{Hanami::VERSION}"
     end
 
     it "prints current version with -v alias" do
-      run_command "hanami -v", "v#{Hanami::VERSION}"
+      run_cmd "hanami -v", "v#{Hanami::VERSION}"
     end
 
     it "prints current version with --version alias" do
-      run_command "hanami --version", "v#{Hanami::VERSION}"
+      run_cmd "hanami --version", "v#{Hanami::VERSION}"
     end
 
     it "prints help message" do
@@ -79,7 +79,7 @@ RSpec.describe "hanami version", type: :integration do
           --help, -h                      	# Print this help
       OUT
 
-      run_command "hanami version --help", output
+      run_cmd "hanami version --help", output
     end
   end
 end
