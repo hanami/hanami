284adfe0952d5b1b4eb0ad0800e99338925b66b6
Tim Riley <tim@icelab.com.au>
2021-05-18 21:44:14 +1000

    Fix typo
    Co-authored-by: Luca Guidi <me@lucaguidi.com>

--- lib/hanami/configuration/router.rb
+++ lib/hanami/configuration/router.rb
@@ -44,7 +44,7 @@ module Hanami
         end
       end
 
-      def respond_to_missing?(name, _incude_all = false)
+      def respond_to_missing?(name, _include_all = false)
         config.respond_to?(name) || super
       end
     end
