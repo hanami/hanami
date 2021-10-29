a4527e87b9b0f5efd63e04ec658e3455b688ddd4
Luca Guidi <me@lucaguidi.com>
2021-10-29 11:18:56 +0200

    Expanded current integration spec for routing helpers

--- spec/integration/routing_helpers_spec.rb
+++ spec/integration/routing_helpers_spec.rb
@@ -30,9 +30,11 @@ EOF
     with_project do
       generate "action web books#index --url=/books"
       generate "action web books#show --url=/books/:id"
+      generate "action web books/reviews#new --url=/books/:id/reviews/new"
 
       # Add `as:` option, so it can be used by the routing helper
       replace "apps/web/config/routes.rb", "/books/:id", "get '/books/:id', to: 'books#show', as: :book"
+      replace "apps/web/config/routes.rb", "/books/:id/reviews/new", "get '/books/:id/reviews/new', to: 'books/reviews#new', as: :new_book_review"
       rewrite "apps/web/views/books/index.rb", <<-EOF
 module Web::Views::Books
   class Index
@@ -48,11 +50,19 @@ EOF
 <h1>Books</h1>
 <h2><a href="<%= featured_book_path %>">Featured Book</a></h2>
 EOF
+      rewrite "apps/web/templates/books/show.html.erb", <<-EOF
+<h1>Book</h1>
+<h2><a href="<%= routes.new_book_review_path(id: params[:id]) %>">New Review</a></h2>
+EOF
 
       server do
         visit "/books"
 
         expect(page.body).to include(%(<a href="/books/23">Featured Book</a>))
+
+        visit "/books/23"
+
+        expect(page.body).to include(%(<a href="/books/23/reviews/new">New Review</a>))
       end
     end
   end
