81dc2986cfcce13398cb9277f337ef64fa889273
Luca Guidi <me@lucaguidi.com>
2022-01-05 15:18:21 +0100

    Refactoring: target `Hanami::Configuration::Logger` as subject class of unit tests

--- spec/unit/hanami/configuration/logger_spec.rb
+++ spec/unit/hanami/configuration/logger_spec.rb
@@ -1,80 +1,93 @@
 # frozen_string_literal: true
 
-require "hanami/configuration"
+require "hanami/configuration/logger"
 require "logger"
 
-RSpec.describe Hanami::Configuration do
-  subject(:config) { described_class.new(application_name: application_name, env: :development) }
-  let(:application_name) { "MyApp::Application" }
+RSpec.describe Hanami::Configuration::Logger do
+  subject { described_class.new(application_name: application_name) }
+  let(:application_name) { :my_app }
 
-  describe "#logger" do
-    describe "#logger_class" do
-      it "defaults to Hanami::Logger" do
-        expect(config.logger.logger_class).to eql Hanami::Logger
-      end
-
-      it "can be changed to another class" do
-        another_class = Class.new
-
-        expect { config.logger.logger_class = another_class }
-          .to change { config.logger.logger_class }
-          .to(another_class)
-      end
+  describe "#logger_class" do
+    it "defaults to Hanami::Logger" do
+      expect(subject.logger_class).to eql Hanami::Logger
     end
 
+    it "can be changed to another class" do
+      another_class = Class.new
+
+      expect { subject.logger_class = another_class }
+        .to change { subject.logger_class }
+        .to(another_class)
+    end
+  end
+
+  describe "#application_name" do
+    it "defaults returns application name" do
+      expect(subject.application_name).to eq(application_name)
+    end
+  end
+
+  describe "#stream" do
+    it "defaults to $stdout" do
+      expect(subject.stream).to eq($stdout)
+    end
+  end
+
+  describe "#stream=" do
+    it "accepts a IO object or a path to a file" do
+      expect { subject.stream = "/dev/null" }
+        .to change { subject.stream }
+        .to("/dev/null")
+    end
+  end
+
+  describe "#options" do
+    it "defaults to {level: :debug}" do
+      expect(subject.options).to eq(level: :debug)
+    end
+
+    it "can have additional options set" do
+      expect { subject.options[:stream] = "/some/file" }
+        .to change { subject.options }
+        .to(level: :debug, stream: "/some/file")
+    end
+
+    it "can be changed to another hash of options" do
+      expect { subject.options = {level: :info} }
+        .to change { subject.options }
+        .to(level: :info)
+    end
+  end
+
+  describe "#filter_params" do
+    it "defaults to a standard array of sensitive param names" do
+      expect(subject.filter_params).to include(*%w[_csrf password password_confirmation])
+    end
+
+    it "can have other params names added" do
+      expect { subject.filter_params << "secret_param" }
+        .to change { subject.filter_params }
+        .to array_including("secret_param")
+    end
+
+    it "can be changed to another array" do
+      expect { subject.filter_params = ["secret_param"] }
+        .to change { subject.filter_params }
+        .to ["secret_param"]
+    end
+  end
+end
+
+
+RSpec.describe Hanami::Configuration do
+  subject(:config) { described_class.new(application_name: application_name, env: env) }
+  let(:application_name) { "MyApp::Application" }
+  let(:env) { :development }
+
+  describe "#logger" do
     describe "#application_name" do
       it "defaults to Hanami::Configuration#application_name" do
         expect(config.logger.application_name).to eq(config.application_name)
-      end
-    end
-
-    describe "#stream" do
-      it "defaults to $stdout" do
-        expect(config.logger.stream).to eq($stdout)
-      end
-    end
-
-    describe "#stream=" do
-      it "accepts a IO object or a path to a file" do
-        expect { config.logger.stream = "/dev/null" }
-          .to change { config.logger.stream }
-          .to("/dev/null")
-      end
-    end
-
-    describe "#options" do
-      it "defaults to {level: :debug}" do
-        expect(config.logger.options).to eq(level: :debug)
-      end
-
-      it "can have additional options set" do
-        expect { config.logger.options[:stream] = "/some/file" }
-          .to change { config.logger.options }
-          .to(level: :debug, stream: "/some/file")
-      end
-
-      it "can be changed to another hash of options" do
-        expect { config.logger.options = {level: :info} }
-          .to change { config.logger.options }
-          .to(level: :info)
-      end
-    end
-
-    describe "#filter_params" do
-      it "defaults to a standard array of sensitive param names" do
-        expect(config.logger.filter_params).to include(*%w[_csrf password password_confirmation])
-      end
-
-      it "can have other params names added" do
-        expect { config.logger.filter_params << "secret_param" }
-          .to change { config.logger.filter_params }
-          .to array_including("secret_param")
-      end
-
-      it "can be changed to another array" do
-        expect { config.logger.filter_params = ["secret_param"] }
-          .to change { config.logger.filter_params }
-          .to ["secret_param"]
       end
     end
   end
