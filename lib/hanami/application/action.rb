ca6007bc7701d3c7b21452d6ea2a33bf68e52eb0
Tim Riley <tim@openmonkey.com>
2022-03-07 00:04:08 +1100

    Go back to the module builder for slice behavior

--- lib/hanami/application/action.rb
+++ lib/hanami/application/action.rb
@@ -2,6 +2,7 @@
 
 require "hanami/action"
 require "hanami/slice_configurable"
+require_relative "action/slice_configured_action"
 
 module Hanami
   class Application
@@ -14,12 +15,6 @@ module Hanami
       extend Hanami::SliceConfigurable
 
       class << self
-        # @api private
-        def inherited(subclass)
-          super
-          subclass.instance_variable_set(:@slice, slice)
-        end
-
         # @api public
         def configuration
           @configuration ||= Application::Action::Configuration.new
@@ -32,67 +27,20 @@ module Hanami
 
         # @api private
         def configure_for_slice(slice)
-          @slice = slice
-
-          actions_config = slice.application.config.actions
-
-          configure_from_actions_config(actions_config)
-          extend_behavior(actions_config)
-        end
-
-        # @api private
-        def slice
-          @slice
-        end
-
-        # @api private
-        def application
-          slice.application
-        end
-
-        private
-
-        def configure_from_actions_config(actions_config)
-          config.settings.each do |setting|
-            config.public_send :"#{setting}=", actions_config.public_send(:"#{setting}")
-          end
-        end
-
-        def extend_behavior(actions_config)
-          if actions_config.sessions.enabled?
-            require "hanami/action/session"
-            include Hanami::Action::Session
-          end
-
-          if actions_config.csrf_protection
-            require "hanami/action/csrf_protection"
-            include Hanami::Action::CSRFProtection
-          end
-
-          if actions_config.cookies.enabled?
-            require "hanami/action/cookies"
-            include Hanami::Action::Cookies
-          end
+          extend SliceConfiguredAction.new(slice)
         end
       end
 
       attr_reader :view, :view_context, :routes
 
-      def initialize(
-        view: resolve_paired_view,
-        view_context: resolve_view_context,
-        routes: resolve_routes,
-        **dependencies
-      )
+      # @see SliceConfiguredAction#define_new
+      # @api public
+      def initialize(view: nil, view_context: nil, routes: nil, **kwargs)
         @view = view
         @view_context = view_context
         @routes = routes
 
-        super(**dependencies)
-      end
-
-      def inspect
-        "#<#{self.class.name}[#{self.class.slice.name}]>"
+        super(**kwargs)
       end
 
       def build_response(**options)
@@ -124,40 +72,6 @@ module Hanami
       # @api public
       def render?(res)
         view && res.body.empty?
-      end
-
-      private
-
-      def resolve_paired_view
-        # There's a lot of class-level things going on... and no instance-level state
-        # required... I wonder if this can move to the class level
-
-        # Is `config` injected to the instance with actions? It might be, meaning I don't
-        # have to reach to the class for it
-        view_identifiers = self.class.config.view_name_inferrer.call(
-          action_name: self.class.name,
-          provider: self.class.slice,
-        )
-
-        view_identifiers.detect do |identifier|
-          break self.class.slice[identifier] if self.class.slice.key?(identifier)
-        end
-      end
-
-      def resolve_view_context
-        identifier = self.class.config.view_context_identifier
-
-        if self.class.slice.key?(identifier)
-          self.class.slice[identifier]
-        elsif self.class.application.key?(identifier)
-          # TODO: we might not need the fallback with the way we're setting up the view layer for slices now
-          self.class.application[identifier]
-        end
-      end
-
-      def resolve_routes
-        # TODO: turn this into a config
-        self.class.application[:routes_helper] if self.class.application.key?(:routes_helper)
       end
     end
   end
---
+++ lib/hanami/application/action/slice_configured_action.rb
@@ -0,0 +1,101 @@
+# frozen_string_literal: true
+
+require "hanami/action"
+require "hanami/slice_configurable"
+
+module Hanami
+  class Application
+    class Action < Hanami::Action
+      # @api private
+      class SliceConfiguredAction < Module
+        attr_reader :slice
+
+        def initialize(slice)
+          @slice = slice
+        end
+
+        def extended(action_class)
+          configure_action(action_class)
+          extend_behavior(action_class)
+          define_new
+        end
+
+        def inspect
+          "#<#{self.class.name}[#{slice.name}]>"
+        end
+
+        private
+
+        # @see Hanami::Application::Action#initialize
+        def define_new
+          resolve_view = method(:resolve_paired_view)
+          resolve_view_context = method(:resolve_view_context)
+          resolve_routes = method(:resolve_routes)
+
+          define_method(:new) do |**kwargs|
+            super(
+              view: kwargs.fetch(:view) { resolve_view.(self) },
+              view_context: kwargs.fetch(:view_context) { resolve_view_context.(self) },
+              routes: kwargs.fetch(:routes) { resolve_routes.() },
+              **kwargs,
+            )
+          end
+        end
+
+        def configure_action(action_class)
+          action_class.config.settings.each do |setting|
+            action_class.config.public_send :"#{setting}=", actions_config.public_send(:"#{setting}")
+          end
+        end
+
+        def extend_behavior(action_class)
+          if actions_config.sessions.enabled?
+            require "hanami/action/session"
+            action_class.include Hanami::Action::Session
+          end
+
+          if actions_config.csrf_protection
+            require "hanami/action/csrf_protection"
+            action_class.include Hanami::Action::CSRFProtection
+          end
+
+          if actions_config.cookies.enabled?
+            require "hanami/action/cookies"
+            action_class.include Hanami::Action::Cookies
+          end
+        end
+
+        def actions_config
+          slice.application.config.actions
+        end
+
+        def resolve_paired_view(action_class)
+          view_identifiers = action_class.config.view_name_inferrer.call(
+            action_name: name,
+            provider: slice,
+          )
+
+          view_identifiers.detect do |identifier|
+            break slice[identifier] if slice.key?(identifier)
+          end
+        end
+
+        def resolve_view_context(action_class)
+          identifier = action_class.config.view_context_identifier
+
+          if slice.key?(identifier)
+            slice[identifier]
+          elsif slice.application.key?(identifier)
+            # TODO: we might not need the fallback with the way we're setting up the view layer for slices now
+            slice.application[identifier]
+          end
+        end
+
+        def resolve_routes
+          # TODO: turn this into a config
+          slice.application[:routes_helper] if slice.application.key?(:routes_helper)
+        end
+      end
+    end
+  end
+end
