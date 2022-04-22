781c648643b0b5e1b7587b7efa83ddc664394e1c
Luca Guidi <me@lucaguidi.com>
2022-04-22 11:00:36 +0200

    Fix previous integration spec

--- spec/new_integration/view/views_spec.rb
+++ spec/new_integration/view/views_spec.rb
@@ -107,23 +107,107 @@ RSpec.describe "Hanami view integration", :application_integration do
         end
       RUBY
 
+      write "lib/test_app/view/base.rb", <<~RUBY
+        require "hanami/application/view"
+
+        module TestApp
+          module View
+            class Base < Hanami::Application::View
+            end
+          end
+        end
+      RUBY
+
+      write "lib/test_app/view/context.rb", <<~RUBY
+        require "hanami/view/context"
+
+        module TestApp
+          module View
+            class Context < Hanami::View::Context
+            end
+          end
+        end
+      RUBY
+
+      write "slices/main/lib/view/base.rb", <<~RUBY
+        require "test_app/view/base"
+
+        module Main
+          module View
+            class Base < TestApp::View::Base
+            end
+          end
+        end
+      RUBY
+
+      write "slices/main/lib/view/context.rb", <<~RUBY
+        require "test_app/view/context"
+
+        module Main
+          module View
+            class Context < TestApp::View::Context
+            end
+          end
+        end
+      RUBY
+
+      write "slices/main/views/users/show.rb", <<~RUBY
+        module Main
+          module Views
+            module Users
+              class Show < View::Base
+                expose :name
+              end
+            end
+          end
+        end
+      RUBY
+
+      write "slices/main/templates/layouts/application.html.slim", <<~SLIM
+        html
+          body
+            == yield
+      SLIM
+
+      write "slices/main/templates/users/show.html.slim", <<~SLIM
+        h1 Hello, \#{name}
+      SLIM
+
+      require "hanami/prepare"
+
+      rendered = Main::Slice["views.users.show"].(name: "Jennifer")
+      expect(rendered.to_s).to eq "<html><body><h1>Hello, Jennifer</h1></body></html>"
+    end
+  end
+
+  specify "Canonical action rendering" do
+    with_tmp_directory(Dir.mktmpdir) do
+      write "config/application.rb", <<~RUBY
+        require "hanami"
+
+        module TestApp
+          class Application < Hanami::Application
+          end
+        end
+      RUBY
+
       write "lib/test_app/action/base.rb", <<~RUBY
-        require "hanami/controller"
+        require "hanami/application/action"
 
         module TestApp
           module Action
-            class Base < Hanami::Action
+            class Base < Hanami::Application::Action
             end
           end
         end
       RUBY
 
       write "lib/test_app/view/base.rb", <<~RUBY
-        require "hanami/view"
+        require "hanami/application/view"
 
         module TestApp
           module View
-            class Base < Hanami::View
+            class Base < Hanami::Application::View
             end
           end
         end
@@ -178,6 +262,9 @@ RSpec.describe "Hanami view integration", :application_integration do
           module Actions
             module Users
               class Show < Action::Base
+                def handle(req, res)
+                  res[:name] = req.params[:name]
+                end
               end
             end
           end
@@ -208,8 +295,8 @@ RSpec.describe "Hanami view integration", :application_integration do
 
       require "hanami/prepare"
 
-      rendered = Main::Slice["views.users.show"].(name: "Jennifer")
-      expect(rendered.to_s).to eq "<html><body><h1>Hello, Jennifer</h1></body></html>"
+      response = Main::Slice["actions.users.show"].(name: "Jennifer")
+      expect(response.body.first).to eq "<html><body><h1>Hello, Jennifer</h1></body></html>"
     end
   end
 end
