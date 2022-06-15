b8935ca46c70a1a6773344cc0bcda645449c9f19
Peter Solnica <peter@solnica.online>
2022-06-15 08:29:24 +0200

    Fix zeitwerk spec helper

--- spec/support/application_integration.rb
+++ spec/support/application_integration.rb
@@ -21,7 +21,16 @@ RSpec.configure do |config|
     # Tear down Zeitwerk (from zeitwerk's own test/support/loader_test)
     Zeitwerk::Registry.loaders.each(&:unload)
     Zeitwerk::Registry.loaders.clear
-    Zeitwerk::Registry.loaders_managing_gems.clear
+
+    # This private interface changes between 2.5.4 and 2.6.0
+    if Zeitwerk::Registry.respond_to?(:loaders_managing_gems)
+      Zeitwerk::Registry.loaders_managing_gems.clear
+    else
+      Zeitwerk::Registry.gem_loaders_by_root_file.clear
+      Zeitwerk::Registry.autoloads.clear
+      Zeitwerk::Registry.inceptions.clear
+    end
+
     Zeitwerk::ExplicitNamespace.cpaths.clear
     Zeitwerk::ExplicitNamespace.tracer.disable
 
