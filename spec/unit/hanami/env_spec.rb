55f0d8ff705454f0106a6ea4d2e60a206be78c6f
Tim Riley <tim@openmonkey.com>
2022-06-19 17:09:06 +1000

    Respect RACK_ENV if HANAMI_ENV is not set (#1168)

---
+++ spec/unit/hanami/env_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+RSpec.describe Hanami, ".env" do
+  subject(:env) { described_class.env }
+
+  before do
+    @orig_env = ENV.to_h
+  end
+
+  after do
+    ENV.replace(@orig_env)
+  end
+
+  context "HANAMI_ENV in ENV" do
+    before do
+      ENV["HANAMI_ENV"] = "test"
+    end
+
+    it "is the value of HANAMI_ENV" do
+      is_expected.to eq :test
+    end
+  end
+
+  context "RACK_ENV in ENV" do
+    before do
+      ENV["RACK_ENV"] = "test"
+    end
+
+    it "is the value of RACK_ENV" do
+      is_expected.to eq :test
+    end
+  end
+
+  context "both HANAMI_ENV and RACK_ENV in ENV" do
+    before do
+      ENV["HANAMI_ENV"] = "test"
+      ENV["RACK_ENV"] = "production"
+    end
+
+    it "is the value of HANAMI_ENV" do
+      is_expected.to eq :test
+    end
+  end
+
+  context "no ENV vars set" do
+    before do
+      ENV.delete("HANAMI_ENV")
+    end
+
+    it "defaults to \"development\"" do
+      is_expected.to eq :development
+    end
+  end
+end
