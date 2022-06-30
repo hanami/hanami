b97eabda8fadc269e5f25c340ec6123b55d69bdc
Peter Solnica <peter@solnica.online>
2022-06-30 07:00:16 +0200

    WIP - repro spec

--- spec/integration/rack_app/middleware_spec.rb
+++ spec/integration/rack_app/middleware_spec.rb
@@ -234,4 +234,113 @@ RSpec.describe "Hanami web app", :app_integration do
       }.to raise_error(Hanami::UnsupportedMiddlewareSpecError)
     end
   end
+
+  specify "Setting middlewares on a per-slice basis in the router" do
+    write "config/app.rb", <<~RUBY
+      require "hanami"
+
+      module TestApp
+        class App < Hanami::App
+          config.logger.stream = File.new("/dev/null", "w")
+        end
+      end
+    RUBY
+
+    write "config/routes.rb", <<~RUBY
+      module TestApp
+        class Middleware
+          def self.called_as
+            @called_as ||= Hash.new { |hsh, key| hsh[key] = [] }
+          end
+
+          def initialize(app, tag)
+            @app = app
+            @tag = tag
+          end
+
+          def call(env)
+            self.class.called_as[@tag] << env["PATH_INFO"]
+            @app.call(env)
+          end
+        end
+
+        class Routes < Hanami::Routes
+          slice :main, at: "/" do
+            use TestApp::Middleware, :main
+
+            root to: "home.show"
+          end
+
+          slice :admin, at: "/admin" do
+            use TestApp::Middleware, :admin
+
+            root to: "home.show"
+
+            get "test", to: "test.show"
+          end
+        end
+      end
+    RUBY
+
+    write "slices/main/actions/home/show.rb", <<~RUBY
+      module Main
+        module Actions
+          module Home
+            class Show < Hanami::Action
+              def handle(*, res)
+                res.body = "Hello from main"
+              end
+            end
+          end
+        end
+      end
+    RUBY
+
+    write "slices/admin/actions/home/show.rb", <<~RUBY
+      module Admin
+        module Actions
+          module Home
+            class Show < Hanami::Action
+              def handle(*, res)
+                res.body = "Hello from admin"
+              end
+            end
+          end
+        end
+      end
+    RUBY
+
+    write "slices/admin/actions/test/show.rb", <<~RUBY
+      module Admin
+        module Actions
+          module Test
+            class Show < Hanami::Action
+              def handle(*, res)
+                res.body = "Hello from admin test"
+              end
+            end
+          end
+        end
+      end
+    RUBY
+
+    require "hanami/boot"
+
+    aggregate_failures do
+      get "/"
+      expect(last_response.status).to eq 200
+      expect(last_response.body).to eq "Hello from main"
+      expect(TestApp::Middleware.called_as).to eq(main: ["/"])
+
+      get "/admin"
+      expect(last_response.status).to eq 200
+      expect(last_response.body).to eq "Hello from admin"
+      expect(TestApp::Middleware.called_as).to eq(main: ["/"], admin: ["/admin"])
+
+      get "/admin/test"
+      expect(last_response.status).to eq 200
+      expect(last_response.body).to eq "Hello from admin test"
+      expect(TestApp::Middleware.called_as).to eq(main: ["/"], admin: ["/admin", "/admin/test"])
+    end
+  end
 end
