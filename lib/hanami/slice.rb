41c809feaa59d500e87242cb2ed50961b10e52bb
Tim Riley <tim@riley.id.au>
2022-07-20 21:58:38 +1000

    Move require into conventional spot
    It’s no longer conditional, so it doesn’t make sense tucked away like this anymore

--- lib/hanami/slice.rb
+++ lib/hanami/slice.rb
@@ -6,6 +6,7 @@ require_relative "constants"
 require_relative "errors"
 require_relative "slice_name"
 require_relative "slice_registrar"
+require_relative "providers/settings"
 
 module Hanami
   # A slice represents any distinct area of concern within an Hanami app.
@@ -337,7 +338,6 @@ module Hanami
           register_provider(:routes, source: Providers::Routes.for_slice(self))
         end
 
-        require_relative "providers/settings"
         if Providers::Settings.settings_defined?(self)
           register_provider(:settings, source: Providers::Settings.for_slice(self))
         end
