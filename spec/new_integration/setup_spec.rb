8a52275235e1f139165c60354702f24da6c35f1d
Tim Riley <tim@riley.id.au>
2022-08-16 10:50:16 +1000

    Move DetectApp logic into single Hanami.app_path

--- spec/new_integration/setup_spec.rb
+++ spec/new_integration/setup_spec.rb
@@ -1,56 +1,145 @@
 # frozen_string_literal: true
 
 RSpec.describe "Hanami setup", :app_integration do
-  shared_examples "hanami setup" do
-    it "requires the app file when found" do
-      with_tmp_directory(Dir.mktmpdir) do
-        write "config/app.rb", <<~RUBY
-          require "hanami"
+  describe "Hanami.setup" do
+    shared_examples "hanami setup" do
+      it "requires the app file when found" do
+        with_tmp_directory(Dir.mktmpdir) do
+          write "config/app.rb", <<~RUBY
+            require "hanami"
 
-          module TestApp
-            class App < Hanami::App
+            module TestApp
+              class App < Hanami::App
+              end
             end
-          end
-        RUBY
+          RUBY
 
-        setup
-
-        expect(Hanami.app?).not_to be(nil)
-      end
-    end
-
-    it "raises when the app file is not found" do
-      with_tmp_directory(Dir.mktmpdir) do
-        expect { setup }.to raise_error Hanami::AppLoadError, /Could not locate your Hanami app file/
-      end
-    end
-
-    it "doesn't raise when the app file is not found but the app is already set" do
-      require "hanami"
-
-      module TestApp
-        class App < Hanami::App
+          expect { setup }.to change { Hanami.app? }.to true
+          expect(Hanami.app).to be TestApp::App
         end
       end
 
-      expect { setup }.not_to raise_error
+      it "requires the app file when found in a parent directory" do
+        with_tmp_directory(Dir.mktmpdir) do
+          write "config/app.rb", <<~RUBY
+            require "hanami"
+
+            module TestApp
+              class App < Hanami::App
+              end
+            end
+          RUBY
+
+          write "lib/foo/bar/.keep"
+
+          Dir.chdir("lib/foo/bar") do
+            expect { setup }.to change { Hanami.app? }.to true
+            expect(Hanami.app).to be TestApp::App
+          end
+        end
+      end
+
+      it "raises when the app file is not found" do
+        with_tmp_directory(Dir.mktmpdir) do
+          expect { setup }.to raise_error Hanami::AppLoadError, /Could not locate your Hanami app file/
+        end
+      end
+
+      it "doesn't raise when the app file is not found but the app is already set" do
+        require "hanami"
+
+        module TestApp
+          class App < Hanami::App
+          end
+        end
+
+        expect { setup }.not_to raise_error
+      end
+    end
+
+    describe "using hanami/setup require" do
+      def setup
+        require "hanami/setup"
+      end
+
+      it_behaves_like "hanami setup"
+    end
+
+    describe "using Hanami.setup method" do
+      def setup(...)
+        require "hanami"
+        Hanami.setup(...)
+      end
+
+      it_behaves_like "hanami setup"
+
+      it "returns the loaded app when the app file is found" do
+        with_tmp_directory(Dir.mktmpdir) do
+          write "config/app.rb", <<~RUBY
+            require "hanami"
+
+            module TestApp
+              class App < Hanami::App
+              end
+            end
+          RUBY
+
+          # Multiple calls return the same app
+          expect(setup).to be(Hanami.app)
+          expect(setup).to be(Hanami.app)
+        end
+      end
+
+      it "returns nil when given `raise_exception: false` and the app file is not found" do
+        with_tmp_directory(Dir.mktmpdir) do
+          expect(setup(raise_exception: false)).to be nil
+        end
+      end
     end
   end
 
-  describe "using hanami/setup require" do
-    def setup
-      require "hanami/setup"
+  describe "Hanami.app_path" do
+    subject(:app_path) { Hanami.app_path }
+
+    context "config/app.rb exists in current directory" do
+      it "returns its absolute path" do
+        with_tmp_directory(Dir.mktmpdir) do
+          write "config/app.rb"
+
+          expect(app_path).to match(%r{^/.*/config/app.rb$})
+        end
+      end
     end
 
-    it_behaves_like "hanami setup"
-  end
+    context "config/app.rb exists in a parent directory" do
+      it "returns its absolute path" do
+        with_tmp_directory(Dir.mktmpdir) do
+          write "config/app.rb"
+          write "lib/foo/bar/.keep"
 
-  describe "using Hanami.setup method" do
-    def setup
-      require "hanami"
-      Hanami.setup
+          Dir.chdir("lib/foo/bar") do
+            expect(app_path).to match(%r{^/.*/config/app.rb$})
+          end
+        end
+      end
     end
 
-    it_behaves_like "hanami setup"
+    context "no app file in any directory" do
+      it "returns nil" do
+        with_tmp_directory(Dir.mktmpdir) do
+          expect(app_path).to be(nil)
+        end
+      end
+    end
+
+    context "directory exists with same name as the app file" do
+      it "returns nil" do
+        with_tmp_directory(Dir.mktmpdir) do
+          write "config/app.rb/.keep"
+
+          expect(app_path).to be(nil)
+        end
+      end
+    end
   end
 end
