e867ee4dd766d6d4b35db23033cd3df83f6a00ac
Marc Busqué <marc@lamarciana.com>
2022-08-16 03:02:14 +0200

    Make setup (finding and loading the app) more robust (#1197)
    Introduce a new `Hanami.setup` method, which is now called when requiring `"hanami/setup"`.
    
    This method will look for the app file (`config/app.rb`) in the current directory, as well as search upwards through parent directories until the app file can be found.
    
    This means that the `hanami` CLI will work as expected even when executed from a deeply nested directory within your Hanami app.
    
    When an app file cannot be found, an `Hanami::AppLoadError` method is raised. This raising behaviour can be disabled by calling `Hanami.setup(raise_exception: false)`.
    
    A secondary `Hanami.app_path` method is also introduced, which is the method that does the actual searching for the app file path. This method either returns the absolute path of the found file, or `nil` if none could be found.

--- lib/hanami.rb
+++ lib/hanami.rb
@@ -9,6 +9,57 @@ module Hanami
   @_mutex = Mutex.new
   @_bundled = {}
 
+  # Finds and loads the Hanami app file (`config/app.rb`).
+  #
+  # Raises an exception if the app file cannot be found.
+  #
+  # @return [Hanami::App] the loaded app class
+  #
+  # @api public
+  # @since 2.0.0
+  def self.setup(raise_exception: true)
+    return app if app?
+
+    app_path = self.app_path
+
+    if app_path
+      require(app_path)
+      app
+    elsif raise_exception
+      raise(
+        AppLoadError,
+        "Could not locate your Hanami app file.\n\n" \
+        "Your app file should be at `config/app.rb` in your project's root directory."
+      )
+    end
+  end
+
+  # Finds and returns the absolute path for the Hanami app file (`config/app.rb`).
+  #
+  # Searches within the given directory, then searches upwards through parent directories until the
+  # app file can be found.
+  #
+  # @param dir [String] The directory from which to start searching. Defaults to the current
+  #   directory.
+  #
+  # @return [String, nil] the app file path, or nil if not found.
+  #
+  # @api public
+  # @since 2.0.0
+  def self.app_path(dir = Dir.pwd)
+    dir = Pathname(dir).expand_path
+    path = dir.join(APP_PATH)
+
+    if path.file?
+      path.to_s
+    elsif !dir.root?
+      app_path(dir.parent)
+    end
+  end
+
+  APP_PATH = "config/app.rb"
+  private_constant :APP_PATH
+
   def self.app
     @_mutex.synchronize do
       unless defined?(@_app)
@@ -22,12 +73,12 @@ module Hanami
   end
 
   def self.app?
-    defined?(@_app)
+    instance_variable_defined?(:@_app)
   end
 
   def self.app=(klass)
     @_mutex.synchronize do
-      if defined?(@_app)
+      if instance_variable_defined?(:@_app)
         raise AppLoadError, "Hanami.app is already configured."
       end
 
--- lib/hanami/setup.rb
+++ lib/hanami/setup.rb
@@ -3,9 +3,4 @@
 require "bundler/setup"
 require "hanami"
 
-begin
-  app_require_path = File.join(Dir.pwd, "config/app")
-  require app_require_path
-rescue LoadError => e
-  raise e unless e.path == app_require_path
-end
+Hanami.setup
