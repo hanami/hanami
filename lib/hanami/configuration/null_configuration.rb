bb79ff8e419348f041c0b7b1743696445c6c9721
Marc Busqué <marc@lamarciana.com>
2022-08-26 13:12:26 +0200

    Configure the router only when hanami-router is present
    That's the same we're doing with views and actions

--- lib/hanami/configuration/null_configuration.rb
+++ lib/hanami/configuration/null_configuration.rb
@@ -5,8 +5,8 @@ require "dry/configurable"
 module Hanami
   class Configuration
     # NullConfiguration can serve as a fallback configuration object when out-of-gem
-    # configuration objects are not available (specifically, when the hanami-controller or
-    # hanami-view gems are not loaded)
+    # configuration objects are not available (specifically, when the
+    # hanami-controller, hanami-router or hanami-view gems are not loaded)
     class NullConfiguration
       include Dry::Configurable
     end
