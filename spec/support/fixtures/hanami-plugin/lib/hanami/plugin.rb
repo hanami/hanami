26d5c66b72a9c2d6a65799e3ebc6b5ba98399419
Marc Busqué <marc@lamarciana.com>
2022-08-30 13:34:01 +0200

    Remove old integration tests

--- spec/support/fixtures/hanami-plugin/lib/hanami/plugin.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Hanami
-  module Plugin
-    require "hanami/plugin/version"
-    require "hanami/plugin/cli"
-  end
-end
--- spec/support/fixtures/hanami-plugin/lib/hanami/plugin/version.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-module Hanami
-  module Plugin
-    VERSION = "0.1.0"
-  end
-end
