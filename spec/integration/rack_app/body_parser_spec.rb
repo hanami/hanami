e0a588ed63aa9a3b44136a0165aa66ed2ab945da
Peter Solnica <peter@solnica.online>
2022-10-04 06:44:20 +0200

    Add a spec for body parser usage

---
+++ spec/integration/rack_app/body_parser_spec.rb
@@ -0,0 +1,62 @@
+# frozen_string_literal: true
+
+require "rack/test"
+
+RSpec.describe "Hanami web app", :app_integration do
+  include Rack::Test::Methods
+
+  let(:app) { Hanami.app }
+
+  around do |example|
+    with_tmp_directory(Dir.mktmpdir, &example)
+  end
+
+  specify "Setting middlewares in the config" do
+    write "config/app.rb", <<~RUBY
+      require "hanami"
+
+      module TestApp
+        class App < Hanami::App
+        end
+      end
+    RUBY
+
+    write "config/routes.rb", <<~RUBY
+      require "hanami/middleware/body_parser"
+
+      module TestApp
+        class Routes < Hanami::Routes
+          use Hanami::Middleware::BodyParser, :json
+          post "/users", to: "users.create"
+        end
+      end
+    RUBY
+
+    write "app/actions/users/create.rb", <<~RUBY
+      module TestApp
+        module Actions
+          module Users
+            class Create < Hanami::Action
+              accept :json
+
+              def handle(req, res)
+                res.body = req.params[:users].join("-")
+              end
+            end
+          end
+        end
+      end
+    RUBY
+
+    require "hanami/boot"
+
+    post(
+      "/users",
+      JSON.dump("users" => %w[jane john jade joe]),
+      "CONTENT_TYPE" => "application/json"
+    )
+
+    expect(last_response).to be_successful
+    expect(last_response.body).to eql("jane-john-jade-joe")
+  end
+end
