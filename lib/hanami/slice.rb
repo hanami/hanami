562a073c07161478df0654fe6f659de2a35d37ea
Tim Riley <tim@riley.id.au>
2022-10-26 06:23:53 +1100

    Add Slice.stop forwarding to container
    This is needed to round out the .prepare and .start methods already forwarding to the container.

--- lib/hanami/slice.rb
+++ lib/hanami/slice.rb
@@ -153,6 +153,10 @@ module Hanami
         container.start(...)
       end
 
+      def stop(...)
+        container.stop(...)
+      end
+
       def key?(...)
         container.key?(...)
       end
