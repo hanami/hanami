1d1b3590c9db0e8d708857d02e8a851071b9fee2
Tim Riley <tim@riley.id.au>
2022-11-01 10:37:00 +1100

    Document Config::Actions::Cookies

--- lib/hanami/config/actions/cookies.rb
+++ lib/hanami/config/actions/cookies.rb
@@ -9,18 +9,45 @@ module Hanami
       # actions, and adds the `enabled?` method to allow app base action to determine whether to
       # include the `Action::Cookies` module.
       #
+      # @api public
       # @since 2.0.0
       class Cookies
+        # Returns the cookie options.
+        #
+        # @return [Hash]
+        #
+        # @api public
+        # @since 2.0.0
         attr_reader :options
 
+        # Returns a new `Cookies`.
+        #
+        # You should not need to initialize this class directly. Instead use
+        # {Hanami::Config::Actions#cookies}.
+        #
+        # @api private
+        # @since 2.0.0
         def initialize(options)
           @options = options
         end
 
+        # Returns true if any cookie options have been provided.
+        #
+        # @return [Boolean]
+        #
+        # @api public
+        # @since 2.0.0
         def enabled?
           !options.nil?
         end
 
+        # Returns the cookie options.
+        #
+        # If no options have been provided, returns an empty hash.
+        #
+        # @return [Hash]
+        #
+        # @api public
         def to_h
           options.to_h
         end
