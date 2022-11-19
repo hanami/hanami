3f554626322f86142091fb8c9a7801c442c9b70c
Peter Solnica <peter@solnica.online>
2022-11-19 10:25:04 +0100

    Quick fix for log dir leftover after running tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+require "pathname"
+
 SPEC_ROOT = File.expand_path(__dir__).freeze
+LOG_DIR = Pathname(SPEC_ROOT).join("..").join("log")
 
 require_relative "support/coverage" if ENV["COVERAGE"].eql?("true")
 
@@ -13,4 +16,13 @@ Hanami::Utils::FileList["./spec/support/**/*.rb"].each do |file|
   next if file.include?("hanami-plugin")
 
   require file
+end
+
+RSpec.configure do |config|
+  config.after(:suite) do
+    # TODO: Find out what causes logger to create this dir when running specs.
+    #       There's probably a test app class being created somewhere with root
+    #       not pointing to a tmp dir.
+    FileUtils.rm_r(LOG_DIR)
+  end
 end
