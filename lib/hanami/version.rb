931ae82df073b4eb68f60aea165821eb00b69726
Luca Guidi <me@lucaguidi.com>
2023-01-30 18:02:31 +0100

    Prepare for v2.0.3

--- lib/hanami/version.rb
+++ lib/hanami/version.rb
@@ -7,7 +7,7 @@ module Hanami
   # @api private
   module Version
     # @api public
-    VERSION = "2.0.2"
+    VERSION = "2.0.3"
 
     # @since 0.9.0
     # @api private
--- spec/unit/hanami/version_spec.rb
+++ spec/unit/hanami/version_spec.rb
@@ -2,6 +2,6 @@
 
 RSpec.describe "Hanami::VERSION" do
   it "returns current version" do
-    expect(Hanami::VERSION).to eq("2.0.2")
+    expect(Hanami::VERSION).to eq("2.0.3")
   end
 end
