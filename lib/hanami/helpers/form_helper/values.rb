aef3c8609a8eb776f4b8f8acaecd686112a8bedb
Tim Riley <tim@riley.id.au>
2023-04-22 21:33:50 +1000

    Tidy Values
    Remove commented out code, don’t needlessly prefix private methods with underscores.

--- lib/hanami/helpers/form_helper/values.rb
+++ lib/hanami/helpers/form_helper/values.rb
@@ -23,12 +23,6 @@ module Hanami
 
         # @since 2.0.0
         # @api private
-        # def initialize(values = {}, csrf_token = nil)
-        #   @values = Utils::Hash.symbolize(values || {})
-        #   @params = values.fetch(:params).to_h
-        #   @csrf_token = csrf_token
-        # end
-
         def initialize(values: {}, params: {}, csrf_token: nil)
           @values = values.to_h
           @params = params.to_h
@@ -41,28 +35,28 @@ module Hanami
         # @since 2.0.0
         # @api private
         def get(*keys)
-          _get_from_params(*keys) || _get_from_values(*keys)
+          get_from_params(*keys) || get_from_values(*keys)
         end
 
         private
 
         # @since 2.0.0
         # @api private
-        def _get_from_params(*keys)
+        def get_from_params(*keys)
           keys.map! { |key| /\A\d+\z/.match?(key.to_s) ? key.to_s.to_i : key }
           @params.dig(*keys)
         end
 
         # @since 2.0.0
         # @api private
-        def _get_from_values(*keys)
+        def get_from_values(*keys)
           head, *tail = *keys
-          result      = @values[head]
+          result = @values[head]
 
           tail.each do |k|
             break if result.nil?
 
-            result = _dig(result, k)
+            result = dig(result, k)
           end
 
           result
@@ -70,10 +64,10 @@ module Hanami
 
         # @since 2.0.0
         # @api private
-        def _dig(base, key)
+        def dig(base, key)
           case base
-          when ::Hash                       then base[key]
-          when Array                        then base[key.to_s.to_i]
+          when ::Hash then base[key]
+          when Array then base[key.to_s.to_i]
           when ->(r) { r.respond_to?(key) } then base.public_send(key)
           end
         end
