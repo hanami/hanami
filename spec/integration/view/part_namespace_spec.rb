abfdc559e32b57b586f2e9d82861b8ea7fc128bb
Tim Riley <tim@riley.id.au>
2023-04-29 10:26:17 +1000

    Improve part_namespace spec
    Stick to generating files to provie the most realistic testing environment.

--- spec/integration/view/part_namespace_spec.rb
+++ spec/integration/view/part_namespace_spec.rb
@@ -4,42 +4,100 @@ require "hanami"
 
 RSpec.describe "App view / Part namespace", :app_integration do
   before do
-    module TestApp
-      class App < Hanami::App
-        config.root = "/test_app"
-      end
-    end
-
-    Hanami.app.instance_eval(&app_hook) if respond_to?(:app_hook)
-    Hanami.app.register_slice :main
-    Hanami.app.prepare
-
-    module TestApp
-      module Views
-        module Parts
-        end
-      end
-    end
-
-    module TestApp
-      class View < Hanami::View
-      end
-    end
-
-    module TestApp
-      module Views
-        module Article
-          class Index < TestApp::View
+    with_directory(make_tmp_directory) do
+      write "config/app.rb", <<~RUBY
+        module TestApp
+          class App < Hanami::App
           end
         end
-      end
+      RUBY
+
+      write "app/view.rb", <<~RUBY
+        # auto_register: false
+
+        require "hanami/view"
+
+        module TestApp
+          class View < Hanami::View
+          end
+        end
+      RUBY
+
+      before_prepare if respond_to?(:before_prepare)
+      require "hanami/prepare"
     end
   end
 
   subject(:part_namespace) { view_class.config.part_namespace }
-  let(:view_class) { TestApp::Views::Article::Index }
 
-  it "is View::Parts" do
-    is_expected.to eq TestApp::Views::Parts
+  describe "app view" do
+    let(:view_class) { TestApp::View }
+
+    describe "no part namespace defined" do
+      it "is nil" do
+        expect(part_namespace).to be nil
+      end
+    end
+
+    describe "part namespace defined" do
+      def before_prepare
+        write "app/views/parts/post.rb", <<~RUBY
+          module TestApp
+            module Views
+              module Parts
+                class Post < Hanami::View::Part
+                end
+              end
+            end
+          end
+        RUBY
+      end
+
+      it "is the Views::Parts namespace within the app" do
+        expect(part_namespace).to eq TestApp::Views::Parts
+      end
+    end
+  end
+
+  describe "slice view" do
+    def before_prepare
+      write "slices/main/view.rb", <<~RUBY
+        # auto_register: false
+
+        module Main
+          class View < TestApp::View
+          end
+        end
+      RUBY
+    end
+
+    let(:view_class) { Main::View }
+
+    describe "no part namespace defined" do
+      it "is nil" do
+        expect(part_namespace).to be nil
+      end
+    end
+
+    describe "part namespace defined" do
+      def before_prepare
+        super
+
+        write "slices/main/views/parts/post.rb", <<~RUBY
+          module Main
+            module Views
+              module Parts
+                class Post < Hanami::View::Part
+                end
+              end
+            end
+          end
+        RUBY
+      end
+
+      it "is the Views::Parts namespace within the slice" do
+        expect(part_namespace).to eq Main::Views::Parts
+      end
+    end
   end
 end
