fb4dc57a15b22012551e604d0b002b6672d950fc
Tim Riley <tim@riley.id.au>
2023-04-29 14:39:07 +1000

    Fix miswordings in test examples

--- spec/integration/view/config/scope_class_spec.rb
+++ spec/integration/view/config/scope_class_spec.rb
@@ -74,14 +74,14 @@ RSpec.describe "App view / Config / Scope class", :app_integration do
       RUBY
     end
 
-    describe "no concrete app scope class defined" do
-      it "generates an app scope class and configures it as the view's scope_class" do
+    describe "no concrete slice scope class defined" do
+      it "generates a slice scope class and configures it as the view's scope_class" do
         expect(view_class.config.scope_class).to be Main::Views::Scope
         expect(view_class.config.scope_class.superclass).to be TestApp::Views::Scope
       end
     end
 
-    describe "concrete app scope class defined" do
+    describe "concrete slice scope class defined" do
       def before_prepare
         super
 
@@ -100,13 +100,13 @@ RSpec.describe "App view / Config / Scope class", :app_integration do
         RUBY
       end
 
-      it "configures the app scope class as the view's scope_class" do
+      it "configures the slice scope class as the view's scope_class" do
         expect(view_class.config.scope_class).to eq Main::Views::Scope
         expect(view_class.config.scope_class).to be_concrete
       end
     end
 
-    context "view not inheriting from app view, no concrete scope class" do
+    context "view not inheriting from app view, no concrete scope class defined" do
       def before_prepare
         write "slices/main/view.rb", <<~RUBY
           # auto_register: false
