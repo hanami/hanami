1406079418fdc0c792c576cd4e74470641473860
Luca Guidi <me@lucaguidi.com>
2023-09-28 10:21:17 +0200

    Fix specs

--- spec/integration/rake_tasks_spec.rb
+++ spec/integration/rake_tasks_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "rake"
-require "hanami/cli"
+require "hanami/cli/bundler"
 
 RSpec.describe "Rake tasks", :app_integration do
   describe "assets:precompile" do
@@ -71,6 +71,37 @@ RSpec.describe "Rake tasks", :app_integration do
 
     context "when hanami-assets is not bundled" do
       let(:hanami_assets_bundled) { false }
+
+      it "raises error" do
+        with_tmp_directory(Dir.mktmpdir) do
+          write "config/app.rb", <<~'RUBY'
+            require "hanami"
+
+            module TestApp
+              class App < Hanami::App
+              end
+            end
+          RUBY
+
+          write "Rakefile", <<~RUBY
+            # frozen_string_literal: true
+
+            require "hanami/rake_tasks"
+          RUBY
+
+          write "app/assets/js/app.js", <<~JS
+            console.log("Hello from index.js");
+          JS
+
+          before_prepare if respond_to?(:before_prepare)
+          require "hanami/prepare"
+
+          expect { Rake::Task["assets:precompile"].invoke }.to raise_error do |exception|
+            expect(exception).to be_a(RuntimeError)
+            expect(exception.message).to match(/Don't know how to build task 'assets:precompile'/)
+          end
+        end
+      end
     end
   end
 end
