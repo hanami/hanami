b717ab48eddf344d7e37bf520aa52ed68d6aeecc
Andrea Fomera <afomera@hey.com>
2025-10-01 00:29:18 -0600

    Add some basic unit specs

---
+++ spec/unit/hanami/slice/router/nested_resource_builder_spec.rb
@@ -0,0 +1,105 @@
+# frozen_string_literal: true
+
+RSpec.describe Hanami::Slice::Router::NestedResourceBuilder do
+  let(:router) { double("router") }
+  let(:parent_path) { "users" }
+  let(:name) { :posts }
+  let(:type) { :plural }
+  let(:options) { {} }
+
+  subject(:builder) do
+    described_class.new(
+      router: router,
+      parent_path: parent_path,
+      name: name,
+      type: type,
+      options: options
+    )
+  end
+
+  describe "#initialize" do
+    it "inherits from ResourceBuilder" do
+      expect(builder).to be_a(Hanami::Slice::Router::ResourceBuilder)
+    end
+
+    it "sets parent_path" do
+      expect(builder.parent_path).to eq "users"
+    end
+
+    it "sets parent_name as singularized parent_path" do
+      expect(builder.parent_name).to eq "user"
+    end
+
+    context "with singular parent" do
+      let(:parent_path) { "profile" }
+
+      it "uses parent_path as parent_name for singular parents" do
+        expect(builder.parent_name).to eq "profile"
+      end
+    end
+  end
+
+  describe "private methods" do
+    describe "#build_route_path" do
+      it "builds nested route path with parent id parameter" do
+        expect(builder.send(:build_route_path, "")).to eq "/users/:user_id/posts"
+      end
+
+      it "builds nested route path with suffix" do
+        expect(builder.send(:build_route_path, "/new")).to eq "/users/:user_id/posts/new"
+      end
+
+      it "builds nested route path with id parameter" do
+        expect(builder.send(:build_route_path, "/:id")).to eq "/users/:user_id/posts/:id"
+      end
+
+      context "with singular nested resource" do
+        let(:type) { :singular }
+        let(:name) { :profile }
+
+        it "removes :id from paths for singular resources" do
+          expect(builder.send(:build_route_path, "/:id/edit")).to eq "/users/:user_id/profile/edit"
+        end
+
+        it "builds correct path without id" do
+          expect(builder.send(:build_route_path, "")).to eq "/users/:user_id/profile"
+        end
+      end
+    end
+
+    describe "#build_route_name" do
+      it "builds nested route name for index action" do
+        expect(builder.send(:build_route_name, :index, "")).to eq "user_posts"
+      end
+
+      it "builds nested route name for other actions" do
+        expect(builder.send(:build_route_name, :show, "")).to eq "user_post"
+      end
+
+      it "builds nested route name with prefix" do
+        expect(builder.send(:build_route_name, :show, "edit_")).to eq "edit_user_post"
+      end
+
+      context "with singular nested resource" do
+        let(:type) { :singular }
+        let(:name) { :profile }
+
+        it "builds correct nested route name for singular resource" do
+          expect(builder.send(:build_route_name, :show, "")).to eq "user_profile"
+        end
+
+        it "builds correct nested route name with prefix for singular resource" do
+          expect(builder.send(:build_route_name, :show, "edit_")).to eq "edit_user_profile"
+        end
+      end
+
+      context "with custom route name" do
+        let(:options) { { as: "article" } }
+
+        it "uses custom route name in nested context" do
+          expect(builder.send(:build_route_name, :show, "")).to eq "user_article"
+        end
+      end
+    end
+  end
+end
---
+++ spec/unit/hanami/slice/router/nested_resource_context_spec.rb
@@ -0,0 +1,93 @@
+# frozen_string_literal: true
+
+RSpec.describe Hanami::Slice::Router::NestedResourceContext do
+  let(:router) { double("router") }
+  let(:parent_path) { "users" }
+
+  subject(:context) do
+    described_class.new(router, parent_path)
+  end
+
+  describe "#initialize" do
+    it "sets router and parent_path" do
+      expect(context.instance_variable_get(:@router)).to eq router
+      expect(context.instance_variable_get(:@parent_path)).to eq parent_path
+    end
+  end
+
+  describe "#resources" do
+    let(:nested_builder) { double("nested_builder") }
+
+    before do
+      allow(Hanami::Slice::Router::NestedResourceBuilder).to receive(:new).and_return(nested_builder)
+      allow(nested_builder).to receive(:build_routes)
+    end
+
+    it "creates a NestedResourceBuilder for plural resources" do
+      expect(Hanami::Slice::Router::NestedResourceBuilder).to receive(:new).with(
+        router: router,
+        parent_path: parent_path,
+        name: :posts,
+        type: :plural,
+        options: { only: [:index] }
+      )
+
+      context.resources(:posts, only: [:index])
+    end
+
+    it "calls build_routes on the nested builder" do
+      expect(nested_builder).to receive(:build_routes)
+      context.resources(:posts)
+    end
+  end
+
+  describe "#resource" do
+    let(:nested_builder) { double("nested_builder") }
+
+    before do
+      allow(Hanami::Slice::Router::NestedResourceBuilder).to receive(:new).and_return(nested_builder)
+      allow(nested_builder).to receive(:build_routes)
+    end
+
+    it "creates a NestedResourceBuilder for singular resources" do
+      expect(Hanami::Slice::Router::NestedResourceBuilder).to receive(:new).with(
+        router: router,
+        parent_path: parent_path,
+        name: :profile,
+        type: :singular,
+        options: { except: [:destroy] }
+      )
+
+      context.resource(:profile, except: [:destroy])
+    end
+
+    it "calls build_routes on the nested builder" do
+      expect(nested_builder).to receive(:build_routes)
+      context.resource(:profile)
+    end
+  end
+
+  describe "private methods" do
+    describe "#build_nested_resource" do
+      let(:nested_builder) { double("nested_builder") }
+
+      before do
+        allow(Hanami::Slice::Router::NestedResourceBuilder).to receive(:new).and_return(nested_builder)
+        allow(nested_builder).to receive(:build_routes)
+      end
+
+      it "creates and builds nested resource" do
+        expect(Hanami::Slice::Router::NestedResourceBuilder).to receive(:new).with(
+          router: router,
+          parent_path: parent_path,
+          name: :comments,
+          type: :plural,
+          options: { only: [:index, :show] }
+        )
+        expect(nested_builder).to receive(:build_routes)
+
+        context.send(:build_nested_resource, :comments, :plural, { only: [:index, :show] })
+      end
+    end
+  end
+end
