08ae605459747b75b5d9e4907bfcc065ecc52560
Tim Riley <tim@riley.id.au>
2026-03-08 16:44:17 +1100

    Introduce Hanami::UniversalLogger (#1567)
    Promote the universal logger from inside the rack logger provider to a top-level class. We’ll soon enhance this to work with a wider range of loggers, and use it from our SQL logging too.

--- lib/hanami/web/rack_logger.rb
+++ lib/hanami/web/rack_logger.rb
@@ -56,87 +56,10 @@ module Hanami
         end
       end
 
-      # @since 2.1.0
-      # @api private
-      class UniversalLogger
-        class << self
-          # @since 2.1.0
-          # @api private
-          def call(logger)
-            return logger if compatible_logger?(logger)
-
-            new(logger)
-          end
-
-          # @since 2.1.0
-          # @api private
-          alias_method :[], :call
-
-          private
-
-          def compatible_logger?(logger)
-            logger.respond_to?(:tagged) && accepts_entry_payload?(logger)
-          end
-
-          def accepts_entry_payload?(logger)
-            logger.method(:info).parameters.any? { |(type, _)| type == :keyrest }
-          end
-        end
-
-        # @since 2.1.0
-        # @api private
-        attr_reader :logger
-
-        # @since 2.1.0
-        # @api private
-        def initialize(logger)
-          @logger = logger
-        end
-
-        # @since 2.1.0
-        # @api private
-        def tagged(*, &blk)
-          blk.call
-        end
-
-        # Logs the entry as JSON.
-        #
-        # This ensures a reasonable (and parseable) representation of our log payload structures for
-        # loggers that are configured to wholly replace Hanami's default logger.
-        #
-        # @since 2.1.0
-        # @api private
-        def info(message = nil, **payload, &blk)
-          logger.info do
-            if blk
-              JSON.generate(blk.call)
-            else
-              payload[:message] = message if message
-              JSON.generate(payload)
-            end
-          end
-        end
-
-        # @see info
-        #
-        # @since 2.1.0
-        # @api private
-        def error(message = nil, **payload, &blk)
-          logger.error do
-            if blk
-              JSON.generate(blk.call)
-            else
-              payload[:message] = message if message
-              JSON.generate(payload)
-            end
-          end
-        end
-      end
-
       # @api private
       # @since 2.0.0
       def initialize(logger, env: :development)
-        @logger = UniversalLogger[logger]
+        @logger = Hanami::UniversalLogger[logger]
         extend(Development) if %i[development test].include?(env)
       end
 
